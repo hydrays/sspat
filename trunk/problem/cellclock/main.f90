53e9f7a145e5858db5856a2f8691299ccfc1faf5
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2011-06-03 20:37:38 +0000

    This is the so-called finetune version in my own code. It suggest that some paremeters are tuned to give a better-looking population structure that (1) when the feedback is large, the SC is small; (2) when the feedback is small, the SC is large.

--- trunk/problem/cellclock/main.f90
+++ trunk/problem/cellclock/main.f90
@@ -3,116 +3,95 @@ program ssa
   use random
   implicit none
 
-  integer i, index
+  integer i, index, itype
   real(kind=8) tau
   real(kind=8) u1, u2, u3
   real(kind=8) tprint
+  real(kind=8) lambda
 
-  type(cell) :: cell_pool(20000) 
-  type(cell) :: cell_pool_old(20000) 
+  type(cell) :: pool(20000) 
+  type(cell) :: pool_old(20000) 
 
   open (unit = 11, file='output', action="write")
 
   call ran_seed(sequence=1234)
-  call pool_init(cell_pool, num)
+  call pool_init(pool, num)
 
   t = 0.0
   tprint = 0.0
   index = 0
+
   do while ( t < tend )
-     cell_pool_old = cell_pool
+     pool_old = pool
      num_old = num
 
-     next_die_time = cell_pool(1)%cellclock
-     next_die_index = 1
+     die_time = pool(1)%clock
+     die_index = 1
      num_cell_can_born = 0
      num_of_tdc = 0
      do i = 1, num
-        if ( cell_pool(i)%cellclock < next_die_time ) then
-           next_die_time = cell_pool(i)%cellclock
-           next_die_index = i
+        if ( pool(i)%clock < die_time ) then
+           die_time = pool(i)%clock
+           die_index = i
         end if
-        if (cell_pool(i)%celltype < 3) then
+        if (pool(i)%type < 3) then
            num_cell_can_born = num_cell_can_born + 1
-        elseif ( cell_pool(i)%celltype .eq. 3 ) then
+        elseif ( pool(i)%type .eq. 3 ) then
            num_of_tdc = num_of_tdc + 1
         end if
      end do
 
-     if (num > L1) then
-        lambda = lambda0*exp(-0.01*(num - L1))
-     else
-        lambda = lambda0
-     end if
-     call expdev(next_born_time)
-     next_born_time = next_born_time/(lambda*num_cell_can_born)
-     tau = min(next_born_time, next_die_time)
+     call get_lambda(num, lambda)
+
+     call expdev(born_time)
+     born_time = born_time/(lambda*num_cell_can_born)
+
+     tau = min(born_time, die_time)
      t =  t + tau
      do i = 1, num
-        cell_pool(i)%cellclock = cell_pool(i)%cellclock - tau
+        pool(i)%clock = pool(i)%clock - tau
      end do
-     if ( next_die_time < next_born_time ) then
-        cell_pool(next_die_index) = cell_pool(num)
-        cell_pool(num)%celltype = 0
-        cell_pool(num)%cellclock = 0.
+
+     if ( die_time < born_time ) then
+        pool(die_index) = pool(num)
+        pool(num)%type = 0
+        pool(num)%clock = 0.
         num = num - 1
-        !print *, t, next_die_time
-!        read(*,*)
      else
-        tau = next_born_time
         call ran2(u1)
         u1 = u1*num_cell_can_born
-        next_born_index = 0
+        born_index = 0
         do while ( u1 .ge. 0 )
-           next_born_index = next_born_index + 1
-           if ( cell_pool(next_born_index)%celltype < 3 ) then
+           born_index = born_index + 1
+           if ( pool(born_index)%type < 3 ) then
               u1 = u1 - 1
            end if
         end do
-        p0 = 1.0/(1.01 + k*real(num_of_tdc)/real(L1))
-        p1 = 0.2!1.0/(1.01 + 2.0*k*real(num_of_tdc)/real(L1))
-        if ( cell_pool(next_born_index)%celltype .eq. 1 ) then
-           call ran2(u3)
-           if ( u3 < 0.8 ) then
-              p = -1.0
-           else
-              p = p0
-           end if
-        elseif ( cell_pool(next_born_index)%celltype .eq. 2 ) then
-           !if ( num < L2 ) then
-           call ran2(u3)
-           if ( u3 < 0.8 ) then
-              p = -1.0
-           else
-              p = p1
-           end if
-        else
-           print *, 'wrong!'
-           read(*,*)
-        end if
+
+        call getp
+
         num = num + 1
-        !print *, 'division', p
-        if ( p .eq. -1 ) then
-           !print *, 'asymm'
-           cell_pool(next_born_index)%cellclock = cell_life_length
-           cell_pool(num)%celltype = cell_pool(next_born_index)%celltype + 1
-           cell_pool(num)%cellclock = cell_life_length      
+        
+        itype = pool(born_index)%type
+        call ran2(u3)
+        if ( u3 < 0.0 ) then
+           pool(born_index)%clock = lifespan(itype)
+           pool(num)%type = pool(born_index)%type + 1
+           pool(num)%clock = lifespan(itype+1)      
         else
            call ran2(u2)
-           if ( u2 > p ) then
-              cell_pool(next_born_index)%celltype = &
-                   cell_pool(next_born_index)%celltype + 1
+           if ( u2 > p(itype) ) then
+              itype = itype + 1
+              pool(born_index)%type = itype
            end if
-           cell_pool(next_born_index)%cellclock = cell_life_length
-           cell_pool(num) = cell_pool(next_born_index)
-           !print *, 'symm division', t, num, next_born_index
-           !read(*, *)
+           pool(born_index)%clock = lifespan(itype)
+           pool(num) = pool(born_index)
         end if
      end if
      if (t > tprint) then
-        !call output_to_file(cell_pool_old, num_old, index)
+        !call output_to_file(pool_old, num_old, index)
         index = index + 1
-        call cell_count(cell_pool, num)
+        call cell_count(pool, num)
         tprint = tprint + 1.0
      end if
   end do
--- trunk/problem/cellclock/setting.f90
+++ trunk/problem/cellclock/setting.f90
@@ -1,30 +1,46 @@
 module paras  
-  use random
-  implicit none
  
-  real(kind=8), parameter :: cell_life_length = 1.0
+  real(kind=8), parameter :: lifespan(3) = (/1.0, 1.0, 1.0/)
   real(kind=8), parameter :: lambda0 = 10.0
-  integer num, num_cell_can_born, next_die_index, next_born_index
+  integer num, num_cell_can_born, die_index, born_index
   integer num_of_tdc, num_old
-  real(kind=8) next_die_time, next_born_time
-  integer next_die_index_old, next_born_index_old
-  real(kind=8) next_die_time_old, next_born_time_old
-  real(kind=8)  lambda, p, t, p0, p1
+  real(kind=8) die_time, born_time
+  !integer die_index_old, born_index_old
+  !real(kind=8) die_time_old, born_time_old
+  real(kind=8)  p(3), t
   real(kind=8), parameter :: tend=500
   real(kind=8), parameter :: L1=2000
-!  real(kind=8), parameter :: L2=1000
-  real(kind=8), parameter :: k=0.1
+  real(kind=8), parameter :: k=0.01
   
   type cell
-     integer celltype
-     real(kind=8) cellclock
+     integer type
+     real(kind=8) clock
   end type cell
 
 contains
+
+  subroutine getp
+    implicit none    
+    p(1) = 0.2 + (0.8 - 0.2)/(1.01 + k*real(num_of_tdc)/real(L1))
+    !p(1) = 1.0/(1.01 + k*real(num_of_tdc)/real(L1))
+    p(2) = 0.4!1.0/(1.01 + 2.0*k*real(num_of_tdc)/real(L1))
+  end subroutine getp
   
-  subroutine output_to_file(cell_pool, num, index)
+  subroutine get_lambda(num, lambda)
     implicit none
-    type (cell), intent(in) :: cell_pool(:)
+    integer, intent(in) :: num
+    real(kind=8), intent(out) :: lambda
+    
+    if (num > L1) then
+       lambda = lambda0*exp(-0.01*(num - L1))
+    else
+       lambda = lambda0
+    end if
+  end subroutine get_lambda
+  
+  subroutine output_to_file(pool, num, index)
+    implicit none
+    type (cell), intent(in) :: pool(:)
     integer, intent(in) :: index, num
     character(30) filename
     integer i, j
@@ -33,22 +49,22 @@ contains
     open (unit = 11, file=filename, action="write")
 
     do i = 1, num
-       write(11, '(I2)', advance="no"), cell_pool(i)%celltype
+       write(11, '(I2)', advance="no"), pool(i)%type
     end do
-    write(11, '(3(F6.2))', advance="no"), lambda, p, t
-    if (next_die_time < next_born_time) then 
-       write(11, '(I8)', advance="no"), next_die_index
+    write(11, '(2(F6.2))', advance="no"), p(0), t
+    if (die_time < born_time) then 
+       write(11, '(I8)', advance="no"), die_index
        write(11, '(I3)', advance="no"), -1
     else
-       write(11, '(I8)', advance="no"), next_born_index
+       write(11, '(I8)', advance="no"), born_index
        write(11, '(I3)', advance="no"), 1
     end if
     close(11)
   end subroutine output_to_file
 
-  subroutine cell_count(cell_pool, num)
+  subroutine cell_count(pool, num)
     implicit none
-    type (cell), intent(in) :: cell_pool(:)
+    type (cell), intent(in) :: pool(:)
     integer, intent(in) :: num
     integer i
     integer num_sc, num_tac, num_tdc
@@ -57,52 +73,53 @@ contains
     num_tac = 0
     num_tdc = 0
     do i = 1, num
-       if ( cell_pool(i)%celltype .eq. 1 ) then 
+       if ( pool(i)%type .eq. 1 ) then 
           num_sc = num_sc + 1
-       elseif ( cell_pool(i)%celltype .eq. 2 ) then 
+       elseif ( pool(i)%type .eq. 2 ) then 
           num_tac = num_tac + 1
-       elseif ( cell_pool(i)%celltype .eq. 3 ) then 
+       elseif ( pool(i)%type .eq. 3 ) then 
           num_tdc = num_tdc + 1
        else
           print *, 'woring'
           read(*,*)
        end if
     end do
-    write(*, '(F10.2, 4I6, F10.4)'), t, num_sc, num_tac, num_tdc, num, p0
+    write(*, '(F10.2, 4I6, F10.4)'), t, num_sc, num_tac, num_tdc, num, p(1)
   end subroutine cell_count
 
-  subroutine pool_print(unit, cell_pool, num)
+  subroutine pool_print(unit, pool, num)
     implicit none
     integer, intent(in) :: unit
-    type (cell), intent(in) :: cell_pool(:)
+    type (cell), intent(in) :: pool(:)
     integer, intent(in) :: num
     
     integer i
     do i = 1, num
-       write(unit, *), cell_pool(i)%celltype
+       write(unit, *), pool(i)%type
     end do
   end subroutine pool_print
 
-  subroutine pool_init(cell_pool, num)
+  subroutine pool_init(pool, num)
+    use random
     implicit none
-    type (cell), intent(out) :: cell_pool(:)
+    type (cell), intent(out) :: pool(:)
     integer, intent(out) :: num
     real(kind=8) u
     real(kind=8) r
 
     integer i
 !!$    num = 20
-!!$    cell_pool(1:5)%celltype = 1
-!!$    cell_pool(6:10)%celltype = 2
-!!$    cell_pool(11:20)%celltype = 3
+!!$    pool(1:5)%type = 1
+!!$    pool(6:10)%type = 2
+!!$    pool(11:20)%type = 3
 !!$    do i = 1, num
 !!$       call ran2(u)
-!!$       cell_pool(i)%cellclock = u*cell_life_length
+!!$       pool(i)%clock = u*lifespan
 !!$    end do
 
     num = 10
-    cell_pool(1:10)%celltype = 1
-    cell_pool(1:10)%cellclock = cell_life_length    
+    pool(1:10)%type = 1
+    pool(1:10)%clock = lifespan(1)    
   end subroutine pool_init
   
 end module paras

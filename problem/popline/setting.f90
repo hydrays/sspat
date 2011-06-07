8f2f1bb015b02cbe030f04347183139793b7569e
hydrays <hydrays@gmail.com>
2011-06-07 01:26:57 +0000

    changed some variables: vdiff->D, deleted v0, D-> H, try to make it simple and consistent with the manuscript. Now I want to do some furthur experiment.

--- problem/popline/setting.f90
+++ problem/popline/setting.f90
@@ -1,23 +1,20 @@
 module setting
-  integer, parameter :: L = 600
-  integer, parameter :: D = 100
-  integer, parameter :: b = 20
+  integer, parameter :: L = 200
+  integer, parameter :: H = 100
+  integer, parameter :: b = 10
   real, parameter :: delta_t = 0.001
   real, parameter :: tend = 5000.0
   real p0(L)
-  real v0(L)
   real p1(L)
-  real, parameter :: v0m = 8.0
-  real, parameter :: v_high = 1.0
-  real, parameter :: vdiff_high = 0.2
-  real, parameter :: vdiff_low = 0.2
+  real, parameter :: v = 1.0
+  real, parameter :: D = 1.0
   real TGFbeta(1-b:L+b)
 
 contains
-  subroutine output_to_file(cell_matrix, index)
+  subroutine output_to_file(cmat, index)
     implicit none
 
-    integer, intent(in) :: cell_matrix(0:L+1, D)
+    integer, intent(in) :: cmat(0:L+1, H)
     integer, intent(in) :: index
     character(30) filename
     integer i, j
@@ -26,12 +23,11 @@ contains
     open (unit = 11, file=filename, action="write")
 
     do i = 1, L
-       do j = 1, D
-          write(11, '(I5)', advance="no"), cell_matrix(i,j)
+       do j = 1, H
+          write(11, '(I5)', advance="no"), cmat(i,j)
        end do
        write(11, '(F15.2)', advance="no"), p0(i)
        write(11, '(F15.2)', advance="no"), TGFbeta(i)
-       write(11, '(F15.2)', advance="no"), v0(i)
        write(11, *)
     end do
     close(11)

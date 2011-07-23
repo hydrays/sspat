def14448611a91480ed2e3a2f01cb476c881ec13
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2011-07-23 01:45:42 +0000

    Paper1: finally I decided to use k1 = 6 instead of k1=6.5 in the 3 feedback model. becuase it is better. So I will switch them back again.

--- trunk/problem/logichem/setting.f90
+++ trunk/problem/logichem/setting.f90
@@ -28,13 +28,13 @@
 module chem_data  
   use nrtype
   implicit none
-  integer(I4B) :: NSample = 1
+  integer(I4B) :: NSample = 1000
   integer(I4B), parameter :: NSpec=5
   integer(I4B), parameter :: NReac=17
   real(kind=8) ap, p0, p1, v0, symp, symp1
   real(kind=8) k1, k2, k3
   real(kind=8), parameter :: L = 500
-  real(kind=8), parameter :: mu = 0.000001
+  real(kind=8), parameter :: mu = 0.00001
 
   integer(I4B), parameter :: Xinit(NSpec)=(/ &
        50, & !SC
@@ -118,8 +118,8 @@ contains
     a(14) = ap*x(4)
     a(15) = ap*x(5)
 
-    a(16) = 0.0*mu*v0*x(1)
-    a(17) = 0.0*mu*x(2)
+    a(16) = mu*v0*x(1)
+    a(17) = mu*x(2)
 
   end subroutine getrate
 

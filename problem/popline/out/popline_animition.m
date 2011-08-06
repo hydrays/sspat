95b41575cd9dba5090421741d849e98658b64a98
hydrays <hydrays@gmail.com>
2011-08-06 19:10:23 +0000

    prepare to test Lander's suggestion, which is to see how fast a mutation with selective advantage can travel

--- problem/popline/out/popline_animition.m
+++ problem/popline/out/popline_animition.m
@@ -7,7 +7,7 @@ mycmap = [1     1    1
 colormap(mycmap)
 
 j = 1;
-for index = 0:5:4055
+for index = 0:1:50
 clf
 s1 = ['0000000' num2str(index)];
 s = s1((length(s1)-4):length(s1));
@@ -20,22 +20,22 @@ popgene = load(gIN);
 D = D - 3;
 i = index + 1
 hold on
-box on;
+%box on;
 % xlim([1,L])
-ylim([-100,100])
+%ylim([0,100])
 
-pcolor(popline(1:L,1:100)')
-axis equal
+pcolor(popline(1:L,1:60)')
+%axis equal
 shading flat
-plot(popgene(:,1), 0.5*D*(popgene(:,2)-1),'-+')
+%plot(popgene(:,1), 0.5*D*(popgene(:,2)-1),'-+')
 
 % plot(1:L, D*popline(1:L, D+1),'m', 'linewidth', lw)
 % plot(1:L, 0.1*popline(1:L, D+2), 'g', 'linewidth', lw)
 % plot(1:L, .5*D*popline(1:L, D+3), 'y', 'linewidth', lw)
-s1 = ['0000000' num2str(index/5)];
+s1 = ['0000000' num2str(index)];
 s = s1((length(s1)-4):length(s1));
 s = s;
-eval(['print(''-dpng'',''select',s,''');'])
+eval(['print(''-dpng'',''initial',s,''');'])
 % eval(['print(''-depsc'',''select',s,''');'])
 genemean(j) = mean(popgene(:,2));
 pause(0.1)

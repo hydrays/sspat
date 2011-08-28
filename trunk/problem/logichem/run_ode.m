407eb721ad7b32e307b3b58ac866c3ba502b8327
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2011-08-28 01:38:29 +0000

    logichem: paper1-continue: first I will test the pm change from 0 to 1 and see how mutant type goes

--- trunk/problem/logichem/run_ode.m
+++ trunk/problem/logichem/run_ode.m
@@ -19,25 +19,64 @@
 % plot(T,Y(:,1),'r-',T,Y(:,2),'-.m',T,Y(:,3),'-g',T,Y(:,4),'-k');
 
 % ===========
-% plot ode trajectory, five species
+% plot ode trajectory, 2 feedback self-recover
 % ===========
+y0 = zeros(3,1);
+y0 = [50 100 350];
+options = odeset('RelTol',1e-4,'AbsTol',[1e-5 1e-5 1e-5]);
+[T1,Y1] = ode45(@funcs2, [0 50],y0,options);
+
+y0 = zeros(5,1);
+y0(1:3) = Y1(length(Y1),:);
+y0(4) = 1;
+y0(5) = 0;
+y0(2) = y0(2) - 1;
+options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5 1e-5 1e-5]);
+[T2,Y2] = ode45(@funcs, [0 200],y0,options);
+T2 = T2 + 50;
+S2 = Y2(:,1) + Y2(:,2) + Y2(:,3) + Y2(:,4) + Y2(:,5);
+S1 = Y1(:,1) + Y1(:,2) + Y1(:,3);
+plot(T2,Y2(:,1),'r-',T2,Y2(:,2),'-b',T2,Y2(:,3),'-g',T2,Y2(:,4),'-k',T2, S2,'-m','linewidth',2)
+hold on
+plot(T1,Y1(:,1),'r-',T1,Y1(:,2),'-b',T1,Y1(:,3),'-g',T1,S1,'-m','linewidth',2)
+xlabel('t','fontsize', 18);
+ylabel('cell population','fontsize',18)
+legend('SC', 'TAC', 'TDC', 'MC','total');
+print('-depsc','fig_logi_ode_2f_selfrecover.eps')
+
+% % ===========
+% % plot ode trajectory, 3 feedback self-recover
+% % ===========
+% y0 = zeros(3,1);
+% y0 = [50 100 350];
+% options = odeset('RelTol',1e-4,'AbsTol',[1e-5 1e-5 1e-5]);
+% [T1,Y1] = ode45(@funcs2, [0 50],y0,options);
+% 
 % y0 = zeros(5,1);
-% y0 = [200 300 600 0.00000000000001 0];
+% y0(1:3) = Y1(length(Y1),:);
+% y0(4) = 1;
+% y0(5) = 0;
+% y0(2) = y0(2) - 1;
 % options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5 1e-5 1e-5]);
-% [T,Y] = ode45(@funcs, [0 600],y0,options);
-% plot(T,Y(:,1),'r-',T,Y(:,2),'-.m',T,Y(:,3),':g',T,Y(:,4),'--k','linewidth',2)
+% [T2,Y2] = ode45(@funcs, [0 200],y0,options);
+% T2 = T2 + 50;
+% S2 = Y2(:,1) + Y2(:,2) + Y2(:,3) + Y2(:,4) + Y2(:,5);
+% S1 = Y1(:,1) + Y1(:,2) + Y1(:,3);
+% plot(T2,Y2(:,1),'r-',T2,Y2(:,2),'-b',T2,Y2(:,3),'-g',T2,Y2(:,4),'-k',T2, S2,'-m','linewidth',2)
+% hold on
+% plot(T1,Y1(:,1),'r-',T1,Y1(:,2),'-b',T1,Y1(:,3),'-g',T1,S1,'-m','linewidth',2)
 % xlabel('t','fontsize', 18);
 % ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'MC');
-% print('-depsc','fig_det_trace.eps')
+% legend('SC', 'TAC', 'TDC', 'MC','total');
+% print('-depsc','fig_logi_ode_3f_selfrecover.eps')
 
-% ===========
-% plot ratio vs pm
-% ===========
-p1m = 0:0.001:1;
-r = zeros(length(p1m),1);
-for i = 1:length(p1m)
-    [i,length(p1m)]
-    r(i) = funcs(p1m(i));
-end
-plot(p1m, r, '-r*')
+% % ===========
+% % plot ratio vs pm
+% % ===========
+% p1m = 0:0.001:1;
+% r = zeros(length(p1m),1);
+% for i = 1:length(p1m)
+%     [i,length(p1m)]
+%     r(i) = funcs(p1m(i));
+% end
+% plot(p1m, r, '-r*')

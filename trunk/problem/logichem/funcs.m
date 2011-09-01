1c9114976dccb242ac0ea42c3dae8fc0e0e00890
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2011-09-01 19:03:34 +0000

    convert back to r26: 2f woundhealing, now added 1f woundhealing. The M-file is also changed.

--- trunk/problem/logichem/funcs.m
+++ trunk/problem/logichem/funcs.m
@@ -39,58 +39,10 @@
 % end
 
 
-% ===========
-% ode, five species 2 feedback
-% ===========
-function dy = funcs(t,y)
-dy = zeros(5,1);    % a column vector
-N = 500;
-k1 = 0.9/N;
-k2 = 4/N;
-vm = 2.5;
-p0 = 1/(1.01+k1*(y(3)+y(5)));
-v = vm/(1+k2*(y(3)+y(5)));
-p1 = 0.45;
-pm = 1.0;
-lambda = max(0, 0.04*(sum(y)-N));
-dy(1) = v*p0*y(1) - v*(1-p0)*y(1) - lambda*y(1);
-dy(2) = 2*v*(1-p0)*y(1) + p1*y(2) - (1-p1)*y(2) - lambda*y(2);
-dy(3) = -0.2*y(3) + 2*(1-p1)*y(2) - lambda*y(3);
-dy(4) = pm*y(4) -(1-pm)*y(4) - lambda*y(4);
-dy(5) = -0.2*y(5) + 2*(1-pm)*y(4) - lambda*y(5);
-end
-
 % % ===========
-% % ode, five species 3 feedback
+% % ode, five species
 % % ===========
 % function dy = funcs(t,y)
-% dy = zeros(5,1);    % a column vector
-% N = 500;
-% k1 = 0.6/N;
-% k2 = 4/N;
-% k3 = 3/N;
-% vm = 2.5;
-% p0 = 1/(1.01+k1*(y(3)+y(5)));
-% v = vm/(1+k2*(y(3)+y(5)));
-% p1 = 0.4;
-% psym = 1/(1+(k3*(y(3)+y(5)))^2);
-% pm = 1.0;
-% lambda = max(0, 0.04*(sum(y)-N));
-% dy(1) = v*psym*p0*y(1) - v*psym*(1-p0)*y(1) - lambda*y(1);
-% dy(2) = v*(1-psym)*y(1) + 2*v*(1-p0)*y(1) + p1*y(2) - (1-p1)*y(2) - lambda*y(2);
-% dy(3) = -0.2*y(3) + 2*(1-p1)*y(2) - lambda*y(3);
-% dy(4) = pm*y(4) -(1-pm)*y(4) - lambda*y(4);
-% dy(5) = -0.2*y(5) + 2*(1-pm)*y(4) - lambda*y(5);
-% end
-
-
-% % ===========
-% % function to get ratio for a ceitain pm
-% % ===========
-% 
-% function y = funcs(pm)
-% 
-% function dy = ode(t,y)
 % dy = zeros(5,1);    % a column vector
 % N = 2000;
 % gain = 0.8/N;
@@ -99,6 +51,7 @@ end
 % p0 = 1/(1.01+gain*(y(3)+y(5)));
 % v = vm/(1+gain2*(y(3)+y(5)));
 % p1 = 0.45;
+% pm = 1.0;
 % lambda = 0.01*(sum(y)-N);
 % dy(1) = v*p0*y(1) - v*(1-p0)*y(1) - lambda*y(1);
 % dy(2) = 2*v*(1-p0)*y(1) + p1*y(2) - (1-p1)*y(2) - lambda*y(2);
@@ -106,24 +59,47 @@ end
 % dy(4) = pm*y(4) -(1-pm)*y(4) - lambda*y(4);
 % dy(5) = -0.1*y(5) + 2*(1-pm)*y(4) - lambda*y(5);
 % end
-% 
-% y0 = zeros(5,1);
-% y0 = [200 300 600 0.00000001 0];
-% options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5 1e-5 1e-5]);
-% [T,Y] = ode45(@ode, [0 1000],y0,options);
-% % plot(T,Y(:,1),'r-',T,Y(:,2),'-.b',T,Y(:,4),'--k',T,Y(:,3),':g',T,Y(:,5),'linewidth',2)
-% % %xlim([600,700])
-% % pause(1)
-% 
-% yratio = 0;
-% t_temp = 0;
-% for i = 1:length(Y)-1
-%     if (T(i) > 200)
-%         yratio = yratio + (T(i+1)-T(i))*(Y(i,4)+Y(i,5))/(sum(Y(i,:)));
-%         t_temp = t_temp + (T(i+1)-T(i));
-%     end
-% end
-% yratio = yratio / t_temp
-% y = yratio;
-% end
 
+
+% ===========
+% function to get ratio for a ceitain pm
+% ===========
+
+function y = funcs(pm)
+
+function dy = ode(t,y)
+dy = zeros(5,1);    % a column vector
+N = 2000;
+gain = 0.8/N;
+gain2 = 4/N;
+vm = 2.5;
+p0 = 1/(1.01+gain*(y(3)+y(5)));
+v = vm/(1+gain2*(y(3)+y(5)));
+p1 = 0.45;
+lambda = 0.01*(sum(y)-N);
+dy(1) = v*p0*y(1) - v*(1-p0)*y(1) - lambda*y(1);
+dy(2) = 2*v*(1-p0)*y(1) + p1*y(2) - (1-p1)*y(2) - lambda*y(2);
+dy(3) = -0.1*y(3) + 2*(1-p1)*y(2) - lambda*y(3);
+dy(4) = pm*y(4) -(1-pm)*y(4) - lambda*y(4);
+dy(5) = -0.1*y(5) + 2*(1-pm)*y(4) - lambda*y(5);
+end
+
+y0 = zeros(5,1);
+y0 = [200 300 600 0.00000001 0];
+options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5 1e-5 1e-5]);
+[T,Y] = ode45(@ode, [0 1000],y0,options);
+% plot(T,Y(:,1),'r-',T,Y(:,2),'-.b',T,Y(:,4),'--k',T,Y(:,3),':g',T,Y(:,5),'linewidth',2)
+% %xlim([600,700])
+% pause(1)
+
+yratio = 0;
+t_temp = 0;
+for i = 1:length(Y)-1
+    if (T(i) > 200)
+        yratio = yratio + (T(i+1)-T(i))*(Y(i,4)+Y(i,5))/(sum(Y(i,:)));
+        t_temp = t_temp + (T(i+1)-T(i));
+    end
+end
+yratio = yratio / t_temp
+y = yratio;
+end
--- trunk/problem/logichem/plot_trace.m
+++ trunk/problem/logichem/plot_trace.m
@@ -1,52 +1,55 @@
 % % ===========
 % % script for 1 feedback
 % % ===========
-% load paperresult_1f_tr_k85
-% tr = paperresult_1f_tr_k85;
+% load tr
+% tr = tr;
 % h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.1)
 % %xlim([0, 600])
 % hold on
 % plot(tr(:,1), tr(:,3), '-b','linewidth', 1.1)
 % plot(tr(:,1), tr(:,4), '-g','linewidth', 1.1)
 % %plot(tr(:,1), tr(:,5), 'k','linewidth', 1.1)
-% plot(tr(:,1), tr(:,2)+tr(:,3)+tr(:,4), '-m','linewidth', 1.1)
+% plot(tr(:,1), tr(:,8), '-m','linewidth', 1.1)
 % xlabel('t','fontsize', 18);
 % ylabel('cell population','fontsize',18)
 % legend('SC', 'TAC', 'TDC', 'total')
 % %set(get(h,'axis'),'FontSize',24)
-% print('-depsc','fig_logi_1f_k85.eps')
+% print('-depsc','fig_logi_1f_k085.eps')
 
 % ===========
-% script for 1 feedback bar-plot
+% script for 1 feedback, woundhealing
 % ===========
-mycmap = [1     0     0
-     0     0     1
-     0     1     0];
-colormap(mycmap)
-load paperresult_1f_tr_k07
-load paperresult_1f_tr_k85
-load paperresult_1f_tr_k1
-tr3 = paperresult_1f_tr_k07;
-tr2 = paperresult_1f_tr_k85;
-tr1 = paperresult_1f_tr_k1;
-
-xpmean(1,:) = mean(tr1(600:1000,2:4));
-xpmean(2,:) = mean(tr2(600:1000,2:4));
-xpmean(3,:) = mean(tr3(600:1000,2:4));
-bar(xpmean,1)
-xlabel('k_1','fontsize', 18);
+load paperresult_tr_1f_wondhealing
+tr = paperresult_tr_1f_wondhealing;
+h = plot(tr(:,1), tr(:,2), '-r','linewidth', 2)
+xlim([0, 200])
+hold on
+plot(tr(:,1), tr(:,3), '-b','linewidth', 2)
+plot(tr(:,1), tr(:,4), '-g','linewidth', 2)
+%plot(tr(:,1), tr(:,5), 'k','linewidth', 1.1)
+%plot(tr(:,1), tr(:,8), '-m','linewidth', 1.1)
+xlabel('t','fontsize', 18);
 ylabel('cell population','fontsize',18)
 legend('SC', 'TAC', 'TDC')
+ylim([0, 600])
 %set(get(h,'axis'),'FontSize',24)
-set(gca,'XTickLabel',{'1', '0.85', '0.7'},'fontsize', 14);
-box on;
-print('-depsc','fig_logi_1f_bar.eps')
-
+print('-depsc','fig_logi_1f_woundhealing.eps')
+figure
+plot(tr(:,1), tr(:,7), '-r','linewidth', 2)
+hold on
+xlim([0, 200])
+ylim([0, 1.4])
+%plot(tr(:,1), tr(:,10), '-g','linewidth', 1.5)
+%plot(tr(:,1), tr(:,11), '-b','linewidth', 1.1)
+legend('p_0','v_0')
+xlabel('t','fontsize', 18);
+ylabel('Stem cell property','fontsize', 18)
+print('-depsc','fig_logi_1f_woundhealing_supp.eps')
 
 % % ===========
 % % script for 2 feedback, woundhealing
 % % ===========
-% load paperresult_tr_2f_wondhealing 
+% load paperresult_tr_2f_wondhealing
 % tr = paperresult_tr_2f_wondhealing;
 % h = plot(tr(:,1), tr(:,2), '-r','linewidth', 2)
 % xlim([0, 200])
@@ -65,6 +68,7 @@ print('-depsc','fig_logi_1f_bar.eps')
 % plot(tr(:,1), tr(:,7), '-r','linewidth', 2)
 % hold on
 % xlim([0, 200])
+% ylim([0, 1.4])
 % plot(tr(:,1), tr(:,10), '-g','linewidth', 1.5)
 % %plot(tr(:,1), tr(:,11), '-b','linewidth', 1.1)
 % legend('p_0','v_0')
@@ -75,10 +79,10 @@ print('-depsc','fig_logi_1f_bar.eps')
 % % ===========
 % % script for 2 feedback, self-recovery
 % % ===========
-% load paperresult_tr_2f_selfrecover
-% tr = paperresult_tr_2f_selfrecover;
+% load tr
+% tr = tr;
 % h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.1)
-% xlim([0, 250])
+% %xlim([0, 250])
 % hold on
 % plot(tr(:,1), tr(:,3), '-b','linewidth', 1.1)
 % plot(tr(:,1), tr(:,4), '-g','linewidth', 1.1)
@@ -91,13 +95,13 @@ print('-depsc','fig_logi_1f_bar.eps')
 % %set(get(h,'axis'),'FontSize',24)
 % print('-depsc','fig_logi_2f_selfrecover.eps')
 
-
+% 
 % % ===========
 % % script for 3 feedback, woundhealing
 % % ===========
 % load paperresult_tr_3f_wondhealing
 % tr = paperresult_tr_3f_wondhealing;
-% h = plot(tr(:,1), tr(:,2), '-r','linewidth', 2)
+% h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.1)
 % xlim([0, 200])
 % hold on
 % plot(tr(:,1), tr(:,3), '-b','linewidth', 2)
@@ -114,19 +118,20 @@ print('-depsc','fig_logi_1f_bar.eps')
 % plot(tr(:,1), tr(:,7), '-r','linewidth', 2)
 % hold on
 % xlim([0, 200])
+% ylim([0, 1.4])
 % plot(tr(:,1), tr(:,10), '-g','linewidth', 2)
 % plot(tr(:,1), tr(:,11), '-b','linewidth', 2)
-% legend('p_0','v_0', 'p_{sym}')
+% legend('p_0','v_0', 'q_0')
 % xlabel('t','fontsize', 18);
 % ylabel('Stem cell property','fontsize', 18)
 % print('-depsc','fig_logi_3f_woundhealing_supp.eps')
 
-
+% 
 % % ===========
 % % script for 3 feedback, self-recovery
 % % ===========
-% load paperresult_tr_3f_selfrecover
-% tr = paperresult_tr_3f_selfrecover;
+% load tr
+% tr = tr;
 % h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.1)
 % xlim([0, 250])
 % hold on
@@ -140,71 +145,6 @@ print('-depsc','fig_logi_1f_bar.eps')
 % ylim([0, 600])
 % %set(get(h,'axis'),'FontSize',24)
 % print('-depsc','fig_logi_3f_selfrecover.eps')
-
-
-% % ===========
-% % script for 1 feedback, mutation
-% % ===========
-% load paperresult_tr_1f_mutation
-% tr = paperresult_tr_1f_mutation;
-% %h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.0)
-% %xlim([0, 250])
-% plot(tr(:,1), tr(:,3), '-b','linewidth', 1.0)
-% hold on
-% plot(tr(:,1), tr(:,4), '-g','linewidth', 1.0)
-% %plot(tr(:,1), tr(:,5), 'k','linewidth', 1.0)
-% %plot(tr(:,1), tr(:,8), '-m','linewidth', 1.0)
-% xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TDC')
-% plot(tr(length(tr),1), tr(length(tr),3), 'k*', 'markersize', 8)
-% ylim([0, 600])
-% %set(get(h,'axis'),'FontSize',24)
-% print('-depsc','fig_logi_1f_mutation.eps')
-
-
-% % ===========
-% % script for 2 feedback, mutation
-% % ===========
-% load paperresult_geneticstability_tr2f 
-% tr = paperresult_geneticstability_tr2f ;
-% h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.0)
-% %xlim([0, 250])
-% hold on
-% plot(tr(:,1), tr(:,3), '-b','linewidth', 1.0)
-% plot(tr(:,1), tr(:,4), '-g','linewidth', 1.0)
-% plot(tr(:,1), tr(:,5), ':k','linewidth', 1.0)
-% plot(tr(:,1), tr(:,6), '-k','linewidth', 1.0)
-% %plot(tr(:,1), tr(:,8), '-m','linewidth', 1.0)
-% xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'MC')
-% plot(tr(length(tr),1), tr(length(tr),2), 'k*', 'markersize', 8)
-% ylim([0, 600])
-% %set(get(h,'axis'),'FontSize',24)
-% print('-depsc','fig_logi_2f_mutation.eps')
-
-
-% 
-% % ===========
-% % script for 3 feedback, mutation
-% % ===========
-% load paperresult_geneticstability_tr3f
-% tr = paperresult_geneticstability_tr3f;
-% h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.0)
-% %xlim([0, 250])
-% hold on
-% plot(tr(:,1), tr(:,3), '-b','linewidth', 1.0)
-% plot(tr(:,1), tr(:,4), '-g','linewidth', 1.0)
-% plot(tr(:,1), tr(:,5), ':k','linewidth', 1.0)
-% plot(tr(:,1), tr(:,6), '-k','linewidth', 1.0)
-% xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'MC')
-% plot(tr(length(tr),1), tr(length(tr),2), 'k*', 'markersize', 8)
-% ylim([0, 600])
-% %set(get(h,'axis'),'FontSize',24)
-% print('-depsc','fig_logi_3f_mutation.eps')
 
 
 % % ===========
@@ -270,20 +210,3 @@ print('-depsc','fig_logi_1f_bar.eps')
 % ylabel('cell population','fontsize',18)
 % legend('SC', 'TAC', 'TDC', 'MC', 'Total')
 % print('-depsc','asym.eps')
-
-% % ===========
-% % script pm
-% % ===========
-% load paperresult_2f_pm
-% load paperresult_3f_pm
-% datapm1 = paperresult_2f_pm;
-% datapm2 = paperresult_3f_pm;
-% h = plot(datapm1(:,1), (datapm1(:,5)+datapm1(:,6))./datapm1(:,7), '-r*','markersize', 10, 'linewidth', 1.1)
-% hold on
-% plot(datapm2(:,1), (datapm2(:,5)+datapm2(:,6))./datapm2(:,7), '-o','markersize', 10, 'linewidth', 1.1)
-% xlabel('p_m','fontsize', 18);
-% ylabel('ratio of mutant type','fontsize',18)
-% ylim([0,1]);
-% legend('feedback on p_0, v_0', 'feedback on p_0, v_0, q_0', 2)
-% %set(get(h,'axis'),'FontSize',24)
-% print('-depsc','fig_logi_pm.eps')

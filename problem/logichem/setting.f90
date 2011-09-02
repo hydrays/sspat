6e210cb0ac11e35c2aeb1ef520f03f0d0fa78e00
hydrays <hydrays@gmail.com>
2011-09-02 01:23:20 +0000

    paper1result: unify parameters: 2f genetic stability

--- problem/logichem/setting.f90
+++ problem/logichem/setting.f90
@@ -14,21 +14,21 @@
 !!$  5: TA -> TA + TC
 !!$  6: TA -> 2 TC
 !!$  7: MC -> 2 MC
-!!$  8: MC -> 2 TMC
+!!$  8: SMC -> 2 SMC
 !!$  9: TC -> 0
-!!$  10: TMC -> 0
+!!$  disable this one: 10: SMC -> 0
 !!$  11: SC -> 0
 !!$  12: TA -> 0
 !!$  13: TC -> 0
 !!$  14: MC -> 0
-!!$  15: TMC -> 0
-!!$  16: SC -> MC
+!!$  15: SMC -> 0
+!!$  16: SC -> SMC
 !!$  17: TAC -> MC
 
 module chem_data  
   use nrtype
   implicit none
-  integer(I4B) :: NSample = 1!00000
+  integer(I4B) :: NSample = 100000
   integer(I4B), parameter :: NSpec=5
   integer(I4B), parameter :: NReac=17
   real(kind=8) ap, p0, p1, v0, symp, symp1
@@ -62,7 +62,7 @@ module chem_data
        (/00, 00, 01, 00, 00/), & !5
        (/00, -1, 02, 00, 00/), & !6
        (/00, 00, 00, 01, 00/), & !7
-       (/00, 00, 00, -1, 02/), & !8
+       (/00, 00, 00, 00, 01/), & !8
        (/00, 00, -1, 00, 00/), & !9
        (/00, 00, 00, 00, -1/), & !10
        (/-1, 00, 00, 00, 00/), & !11
@@ -81,17 +81,14 @@ contains
     real(kind=8), intent(in) :: x(NSpec)
     real(kind=8), intent(out) :: a(NReac)
     real(kind=8), intent(in) :: pm
-    k1 = 1.0
-    !k2 = 4.0
+    k1 = 0.9
+    k2 = 4.0
     !k3 = 3.0
     p1 = 0.4
-    p0 = 1.0/(1.01 + k1*(x(3)+x(5))/L)
-    !p0 = 1.0/(1.01 + k1*(x(3)+x(5))/L)
-    v0 = 1.0
-    !v0 = 2.5/(1.0 + k2*(x(3)+x(5))/L)
-    !p0 = 1.0/(1.01 + kappa*(1450)/L)
-    !v0 = 2.5/(1.0 + k2*(1450.0)/L)
-    symp = 1.0!/(1.0 + (k3*(x(3)+x(5))/L)**2)
+    p0 = 1.0/(1.01 + k1*x(3)/L)
+    !v0 = 1.0
+    v0 = 2.5/(1.0 + k2*x(3)/L)
+    symp = 1.0!/(1.0 + (k3*x(3)/L)**2)
     symp1 = 1.0
     if (sum(x) > L) then
        ap = 0.04*(sum(x) - L)
@@ -106,15 +103,14 @@ contains
     a(6) = symp1*x(2)*(1.0-p1)
 
     a(7) = x(4)*pm
-    a(8) = x(4)*(1.0-pm)
+    a(8) = x(5)*v0*pm
 
     a(9) = 0.2*x(3)
-    a(10) = 0.2*x(5)
+    a(10) = 0.0*x(5)
 
     a(11) = ap*x(1)
     a(12) = ap*x(2)
     a(13) = ap*x(3)
-
     a(14) = ap*x(4)
     a(15) = ap*x(5)
 

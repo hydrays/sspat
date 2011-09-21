80ca51654be62eafe04c9e8e8fb40b1c606f3fc4
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2011-09-21 22:36:04 +0000

    pkmc: the initial setup, still wrong. The TGFbeta get wrong in the boundary. Now consider about aboundon the book-keeping of the TGFbeta concentration. Instead, we book-keep the TDC number at each stack (similar as npack), with which we compute p0 on the fly.

---
+++ trunk/problem/pkmc/main.f90
@@ -0,0 +1,139 @@
+program main
+  use omp_lib
+  use nrtype
+  use random
+  use setting
+  implicit none
+
+  real t, tau, tp, tm, u
+  integer output_index, i, j, active_index, temp_num
+  real tau_temp
+  integer k
+  open (unit = 100, file='./out/logfile', action="write")
+  call ran_seed(sequence=12345)
+
+  cmat(1:L, 1)%type = 1
+  cmat(1:L, 2:3)%type = 2
+  cmat(1:L, 3:4)%type = 3
+!  cmat(1:L, 5:50)%type = 1
+  do i = 1, L
+     call ran2(u)
+     cmat(i, 1)%gene1 = 0.5*u
+  end do
+
+  t = 0.0
+  tp = 0.0
+  output_index = 0
+  NP = 0.0
+  NT = 0.0
+  do i = 1, L
+     call expdev(u)
+     NP(i) = u
+  end do
+  cmat(0, :) = cmat(L, :)
+  cmat(L+1, :) = cmat(1, :)
+
+  npack = 0
+  do i = 0, L+1
+     do j = 1, H
+        if ( cmat(i,j)%type .ne. 0 ) then
+           npack(i) = npack(i) + 1
+        end if
+     end do
+  end do
+     
+  TGFbeta = 0.
+  do i = 1, L
+     temp_num = 0.
+     do j = 1, H
+        if ( cmat(i,j)%type .eq. 3 ) then
+           temp_num = temp_num + 1.
+        end if
+     end do
+     !temp_num = 10.0*temp_num / b
+     if (temp_num > 0) then
+        do j = 0, b
+           if ( j .eq. 0) then
+              TGFbeta(i) = TGFbeta(i) + temp_num
+           else
+              TGFbeta(i+j) = TGFbeta(i+j) + temp_num*exp(-real(j)/b)
+              TGFbeta(i-j) = TGFbeta(i-j) + temp_num*exp(-real(j)/b)
+           end if
+        end do
+     end if
+  end do
+  do j = 1, b
+     TGFbeta(j) = TGFbeta(j) + TGFbeta(L+j)
+     TGFbeta(L-j+1) = TGFbeta(L-j+1) + TGFbeta(1-j)
+  end do
+  TGFbeta(1-b:0) = TGFbeta(L-b+1:L)
+  TGFbeta(L+1:L+b) = TGFbeta(1:b)
+
+  D_TGFbeta = 0.0
+  do j = 1, 1+2*b
+     D_TGFbeta(j) = exp(-real(abs(j-b-1))/b)
+  end do
+  
+  do while (t < tend)
+     if (t .ge. tp) then
+        call output_to_file(output_index)
+        call cell_stat(t)
+        print *, t
+        output_index = output_index + 1
+        tp = tp + 5.0
+     end if
+
+     tau = 11111111111.1
+     k = 0
+     do i = 1, L
+        vr = exp(real(npack(i)-npack(i+1)))
+        vl = exp(real(npack(i)-npack(i-1)))
+        a(i) = vr + vl + npack(i)*v
+        tau_temp = ( NP(i) - NT(i) ) / a(i)
+        if ( tau_temp < tau) then
+           tau = tau_temp
+           k = i
+        end if
+     end do
+     if ( k .le. 0 ) then
+        write(*,*), 'error'
+        read(*,*)
+     end if
+     do i = 1, L
+        NT(i) = NT(i) + a(i)*tau
+     end do
+     call expdev(u)
+     NP(k) = NP(k) + u
+     t = t + tau
+     call cell_event(k)
+     ! perodic boundary condition
+     if ( k .eq. 1 ) then
+        cmat(L, :) = cmat(0, :)
+        cmat(L+1, :) = cmat(1, :)
+        npack(L) = npack(0)
+        npack(L+1) = npack(1)
+     end if
+     if ( k .eq. 2 ) then
+        cmat(L+1, :) = cmat(1, :)
+        npack(L+1) = npack(1)
+     end if
+     if ( k .eq. L ) then
+        cmat(1, :) = cmat(L+1, :)
+        cmat(0, :) = cmat(L, :)
+        npack(1) = npack(L+1)
+        npack(0) = npack(L)
+     end if
+     if ( k .eq. L-1 ) then
+        cmat(0, :) = cmat(L, :)
+        npack(0) = npack(L)
+     end if
+     if ( k .le. 2*b+1 ) then
+        TGFbeta(L-b:L+b+1) = TGFbeta(-b:b+1)
+     end if
+     if ( k .ge. L - 2*b ) then
+        TGFbeta(-b:b+1) = TGFbeta(L-b:L+b+1)
+     end if
+  end do
+
+  close(unit=100)
+end program main

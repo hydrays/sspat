1adf524af9ac6219d999973c1d83380ffb30d833
hydrays <hydrays@gmail.com>
2011-11-12 18:01:56 +0000

    pkmc64: by reducing the openmp barrelel time from 0.01 to 0.001, the mesh grid difference is gone. Note that the mesh difference can only be observed when the hills are steap.

--- problem/pkmc64_3d/main.f90
+++ problem/pkmc64_3d/main.f90
@@ -6,19 +6,29 @@ program main
 
   real t, tau, tp, tm, u, private_t
   integer output_index, i, j, active_index
-  integer k1, k2, shift_i, shift_j, iredblack
-  integer ilow, iup, jlow, jup, nthread, npar, scanner
+  integer k1, k2, shift_i, shift_j 
+  integer ibl, jbl
+  integer ilow, iup, jlow, jup
+  integer nthread, npar, scanner
   real(4) r
   integer :: grainsize = 64
   integer, allocatable :: seed(:)
+  real Lbox_npar, Lbox_2npar
+  integer ibox, jbox
 
-  npar = 1
-  allocate(seed(npar))
-  do i = 1,npar
+  npar = 4
+  Lbox_npar = Lbox/npar
+  Lbox_2npar = Lbox/(2*npar)
+  
+  !print *, Lbox_npar, Lbox_2npar
+  !read(*,*)
+
+  allocate(seed(2**npar))
+  do i = 1,2**npar
      call random_number(r)
      seed(i) = 123456789*r
   enddo
-  call par_zigset(npar, seed, grainsize)
+  call par_zigset(2**npar, seed, grainsize)
 
   open (unit = 100, file='./out/logfile', action="write")
   !call ran_seed(sequence=12345)
@@ -32,16 +42,19 @@ program main
   output_index = 0
   scanner = 1
 
-  !  call omp_set_num_threads(int(npar, 4))
+  !print *, int(2**npar,4)
+  !read(*,*)
+
+  call omp_set_num_threads(int(2**npar, 4))
 
   do while (t < tend)
      if (t .ge. tp) then
         call output_to_file(output_index)
         call cell_stat(t)
         output_index = output_index + 1
-        tp = tp + 1.0
+        tp = tp + 2.0
      end if
-     t = t + 0.01
+     t = t + 0.004
 
 !!$     if (t .ge. tm) then
 !!$        do i = 900, 1100
@@ -52,74 +65,79 @@ program main
 !!$        tm = huge(1.0)
 !!$     end if
 
-     !!#do iredblack = 0, 1
-     !$OMP PARALboxLboxELbox default(private) &
-     !!#shared(a, NT, NP, t, npack, TDC, cmat, iredblack, scanner, npar)
+     do jbl = 0, 1
+        do ibl = 0, 1
+           !$OMP PARALLEL default(private) &
+           shared(a, NT, NP, t, npack, TDC, cmat, ibl, jbl, scanner, npar, Lbox_npar, Lbox_2npar)
 
-     !!#nthread = OMP_GET_THREAD_NUM()
-     !!#ilow = (nthread)*Lbox/npar + iredblack*Lbox/(2*npar) + 1
-     !!#iup = ilow + Lbox/(2*npar) - 1
+           nthread = OMP_GET_THREAD_NUM()
+           ibox = nthread/npar
+           jbox = mod(nthread, npar)
 
-     nthread = 0
-     ilow = 1
-     iup = Lbox
-     jlow = 1
-     jup = Lbox
+           !print *, nthread, ibox, jbox
+           !read(*,*)
 
-     !print *, nthread, ilow, iup
-     private_t = t
-     !print *, nthread, private_t, t
-     !read(*,*)
-     do while ( private_t < t + 0.01)
-        call Next_Reaction(k1, k2, tau, ilow, iup, jlow, jup)
-        !print *, 'next', k1, k2
-        call cell_event(k1, k2, nthread)
-        if ( (k1 .le. 2).or.(k1 .ge. Lbox-1) ) then
-           call Perodic_BC(k1,k2)
-        end if
-        if ( (k2 .le. 2).or.(k2 .ge. Lbox-1) ) then
-           call Perodic_BC(k1,k2)
-        end if
+           ilow = ibox*Lbox_npar + ibl*Lbox_2npar + 1
+           iup = ilow + Lbox_2npar - 1
 
-        do i = ilow, iup
-           do j = jlow, jup
-              NT(i,j) = NT(i,j) + a(i,j)*tau
-           end do
-        end do
+           jlow = jbox*Lbox_npar + jbl*Lbox_2npar + 1
+           jup = jlow + Lbox_2npar - 1
 
-        do i = -2, 2
-           do j = -2, 2
-              shift_i = k1 + i
-              shift_j = k2 + j
-              if ( shift_i .le. 0 ) then
-                 shift_i = shift_i + Lbox
-              else if ( shift_i > Lbox ) then
-                 shift_i = shift_i - Lbox
+           !write(*, '(8I6)'), nthread, ibox, jbox, ilow, iup, jlow, jup
+           private_t = t
+           !print *, nthread, private_t, t
+           !read(*,*)
+           do while ( private_t < t +  0.004)
+              call Next_Reaction(k1, k2, tau, ilow, iup, jlow, jup)
+              !print *, 'next', k1, k2
+              call cell_event(k1, k2, nthread)
+              if ( (k1 .le. 2).or.(k1 .ge. Lbox-1) ) then
+                 call Perodic_BC(k1,k2)
+              end if
+              if ( (k2 .le. 2).or.(k2 .ge. Lbox-1) ) then
+                 call Perodic_BC(k1,k2)
               end if
 
-              if ( shift_j .le. 0 ) then
-                 shift_j = shift_j + Lbox
-              else if ( shift_j > Lbox ) then
-                 shift_j = shift_j - Lbox
+              do i = ilow, iup
+                 do j = jlow, jup
+                    NT(i,j) = NT(i,j) + a(i,j)*tau
+                 end do
+              end do
+
+              do i = -2, 2
+                 do j = -2, 2
+                    shift_i = k1 + i
+                    shift_j = k2 + j
+                    if ( shift_i .le. 0 ) then
+                       shift_i = shift_i + Lbox
+                    else if ( shift_i > Lbox ) then
+                       shift_i = shift_i - Lbox
+                    end if
+
+                    if ( shift_j .le. 0 ) then
+                       shift_j = shift_j + Lbox
+                    else if ( shift_j > Lbox ) then
+                       shift_j = shift_j - Lbox
+                    end if
+                    call Update_Rate(shift_i, shift_j)
+                 end do
+              end do
+              u = par_uni(nthread)
+              if ( u.le.0 .or. u.ge.1) then
+                 print *, 'haha ran error'
+                 stop
               end if
-              call Update_Rate(shift_i, shift_j)
+              u = -log(u)
+              NP(k1, k2) = NP(k1, k2) + u
+              private_t = private_t + tau
+              !print *, 'event', k1, k2, npack(k1, k2)
            end do
+           !$OMP END parallel
+           !$OMP barrier
         end do
-        u = par_uni(nthread)
-        if ( u.le.0 .or. u.ge.1) then
-           print *, 'haha ran error'
-           stop
-        end if
-        u = -log(u)
-        NP(k1, k2) = NP(k1, k2) + u
-        private_t = private_t + tau
-        !print *, 'event', k1, k2, npack(k1, k2)
+        !read(*,*)
+        !scanner = scanner + 1
      end do
-     !$OMP END parallel
-     !$OMP barrier
   end do
-  !read(*,*)
-  !scanner = scanner + 1
-!end do
   close(unit=100)
 end program main

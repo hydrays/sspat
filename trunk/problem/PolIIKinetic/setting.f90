5eb150209e0c32ad92dac0b46248cfad24635923
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-01-18 22:09:54 +0000

    PolII: Model 3, now make them all represented by reactions.

--- trunk/problem/PolIIKinetic/setting.f90
+++ trunk/problem/PolIIKinetic/setting.f90
@@ -2,37 +2,38 @@
 
 !!$ >>> NSpec = 3
 
-!!$ (2,  3,  4)
+!!$ (1,  2,  2)
 !!$  C   R   E
 
 !!$ >>> NReac = 5
 
-!!$  1: 0 -> C
+!!$  1: I -> C
 !!$  2: C -> R
 !!$  3: R -> E
-!!$  4: E -> C
-!!$  5: E -> 0
+!!$  4: R -> E + C
+!!$  5: E -> RNA
 
 module chem_data  
   use nrtype
   implicit none
-  integer(I4B) :: NSample = 1
+  integer(I4B) :: NSample = 10000
   integer(I4B), parameter :: NSpec=3
   integer(I4B), parameter :: NReac=5
   real(kind=8) s1, s2, s3
+  real(kind=8), parameter :: ep = 0.0
 
   integer(I4B), parameter, dimension(NSpec,NReac) :: nu = reshape( &
        source=(/ &
        (/01, 00, 00/), & !1
        (/-1, 01, 00/), & !2
        (/00, -1, 01/), & !3
-       (/01, 00, -1/), & !4
+       (/01, -1, 01/), & !4
        (/00, 00, -1/) &  !5
        /), shape = (/NSpec, NReac/) &
        )
 
-  real(kind=8), parameter, dimension(NReac) :: c =  &
-       (/0.02, 0.2, 2.0, 0.1, 0.01/)
+  real(kind=8), parameter, dimension(4) :: c =  &
+       (/0.02, 0.2, 100.0, 0.1/)
 
   real(kind=8), parameter, dimension(NSpec) :: xinit =  &
        (/0.0, 0.0, 0.0/)
@@ -50,14 +51,15 @@ contains
     else
        is_avai = -1.0
        print *, 'wrong occupy'
+       print *, x
        read(*,*)
     end if
 
     a(1) = s1*c(1)*is_avai
     a(2) = s2*c(2)*x(1)
-    a(3) = s3*c(3)*x(2)
-    a(4) = c(4)*x(3)*is_avai
-    a(5) = c(5)*x(3)
+    a(3) = ep*s3*c(3)*x(2)
+    a(4) = (1-ep)*s3*c(3)*x(2)
+    a(5) = c(4)*x(3)
 
   end subroutine getrate
 

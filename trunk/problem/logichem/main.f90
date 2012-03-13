c76b53171b0a9e8ebff4ea345dfd3c92f1aafe12
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-03-13 02:00:55 +0000

    Rolled back to r102.

--- trunk/problem/logichem/main.f90
+++ trunk/problem/logichem/main.f90
@@ -6,31 +6,24 @@ program ssa
 
   real(kind=8) x(NSpec)
   real(kind=8) te
-  real(kind=8) a(NReac), cuma(NReac), u, u1, u2
+  real(kind=8) a(NReac), cuma(NReac), u
   real(kind=8) delta_t, t, tp, td, tac_mtime, sc_mtime
   integer(I4B) is_nag, tac_mflag, sc_mflag
   integer(I4B) i, j, k, index
-  real(kind=8) N_mutation
+  real(kind=8) pm, N_mutation
   real(kind=8) xbar(NSpec), tbar, xbar_counter
 
-  call ran_seed(sequence=12341)
-  te = 1000.0
+  call ran_seed(sequence=1234)
+  te = 200.0
   !te = huge(1.0)
-  !pm = 1.0
-  !vmut = 1.0
+  pm = 1.0
+  vmut = 1.0
 
-!  do pm = 0.0, 1.0001, 0.02
+  do pm = 0.0, 1.0001, 0.02
 !  do vmut = 0.1, 3.1, 0.1
 !  xbar = 0.0
   do index = 1.0, NSample
-     x = 0
-     x(1) = 20
-     x(2) = 30
-     x(3) = 150
-
-     pm = 0.0
-     vmut = 0.0
-     
+     x = xinit
      t = 0.0
      tp = 0.0
      td = 100.0
@@ -41,7 +34,7 @@ program ssa
      sc_mtime = 0.0
      xbar_counter = 0.0
      do while(t < te)
-        call getrate(x, a)
+        call getrate(x, a, pm)
         cuma = a
         do j=2, NReac
            cuma(j) = cuma(j-1) + a(j)
@@ -56,42 +49,10 @@ program ssa
            j = j+1
         end do
         x = x + nu(:, j)
-        if (j.eq.41) then
-           call ran2(u1)
-           call ran2(u2)
-           u1 = 0.5 + (1.0-0.5)*u1
-           u2 = 0.5 + (2.0-0.5)*u2
-!           print *, 'mutation', u1, u2
-!           read(*,*)
-           if ( x(4) .eq. 0 ) then
-              pm(1) = u1
-              vmut(1) = u2
-              x(4) = 1
-           else if ( x(6) .eq. 0 ) then
-              pm(2) = u1
-              vmut(2) = u2
-              x(6) = 1
-           else if ( x(8) .eq. 0 ) then
-              pm(3) = u1
-              vmut(3) = u2
-              x(8) = 1
-           else if ( x(10) .eq. 0 ) then
-              pm(4) = u1
-              vmut(4) = u2
-              x(10) = 1
-           else if ( x(12) .eq. 0 ) then
-              pm(5) = u1
-              vmut(5) = u2
-              x(12) = 1
-           else if ( x(14) .eq. 0 ) then
-              pm(6) = u1
-              vmut(6) = u2
-              x(14) = 1
-           else
-              print *, 'too many mcs...'
-              read(*,*)
-           end if
-        end if
+!!$        if (j.eq.16) then
+!!$           print *, x
+!!$           read(*,*)
+!!$        end if
         call checkx(x, is_nag)
         if (is_nag .eq. 1) then
            print *, 'nag'
@@ -109,24 +70,21 @@ program ssa
 !!$           exit
 !!$        end if
 !!$        if(t > td) then
-!!$!           if (x(4).eq.0) x(4) = 1
-!!$           x(3) = 20
+!!$           if (x(4).eq.0) x(4) = 1
 !!$           td =  td + 200.0
 !!$        end if
-!!$        if(t > tp) then
-!!$           write (*, '(F10.2, 20F8.2)'), t, x, sum(x), ap, p0, v0
-!!$           tp =  tp + 1.0
-!!$           if (t > 1000) then
-!!$           end if
-!!$        end if
+        if(t > tp) then
+           write (*, '(F10.2, 10F8.2)'), t, x, sum(x), ap, p0, v0
+           tp =  tp + 1.0
+           if (t > 1000) then
+           end if
+        end if
         !if (x(1).eq.0 .or. x(5).ne.0 .or. x(4).ne.0) then 
         !if (x(1).eq.0 .or. x(5).ne.0) then 
-        if (sum(x(1:3)).eq.0) then 
-           write (*, '(F10.2, 20F10.4)'), t, x, sum(x), ap, &
-                (pm(1)*x(4)+pm(2)*x(6)+pm(3)*x(8)+pm(4)*x(10)+pm(5)*x(12)+pm(6)*x(14))/(x(4)+x(6)+x(8)+x(10)+x(12)+x(14)), &
-                (vmut(1)*x(4)+vmut(2)*x(6)+vmut(3)*x(8)+vmut(4)*x(10)+vmut(5)*x(12)+vmut(6)*x(14))/(x(4)+x(6)+x(8)+x(10)+x(12)+x(14))
-           exit
-        end if
+!!$        if (sum(x(1:3)).eq.0) then 
+!!$           write (*, '(F10.2, 10F8.2)'), t, x, sum(x), ap, p0, v0           
+!!$           exit
+!!$        end if
      end do
      xbar = xbar + x
 !     write (*, '(F10.2, 9F8.2, 2I8, 2F10.2)'), t, x, sum(x), ap, p0, v0, &
--- trunk/problem/logichem/plot_trace.m
+++ trunk/problem/logichem/plot_trace.m
@@ -1,53 +1,20 @@
-% % ===========
-% % test
-% % ===========
-% load tr
-% clf;
-% tr = tr;
-% plot(tr(:,1), tr(:,2), 'r')
-% %xlim([150, 350])
-% hold on
-% plot(tr(:,1), tr(:,3), 'b')
-% plot(tr(:,1), tr(:,4), 'g')
-% plot(tr(:,1), tr(:,5), 'k')
-% % figure
-% plot(tr(:,1), tr(:,7), 'm')
-% xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'MC', 'Total')
-
-% % ===========
-% % test for mutation
-% % ===========
-% load pr2_random_mutation_sample1
-% load pr2_random_mutation_sample2
-% load pr2_random_mutation_sample3;
-% load sam4
-% sam1 = pr2_random_mutation_sample1;
-% sam2 = pr2_random_mutation_sample2;
-% sam3 = pr2_random_mutation_sample3;
-% load sam1
-% load sam2
-% load sam3
-% load sam4
-% clf;
-% 
-% plot(sam2(1:1000,19), sam2(1:1000,20), '*') 
-% xlim([0.5,1])
-% title('v_0 change, MTDCs feedback'), xlabel('p_m'), ylabel('v_m')
-% subplot(2,2,1),...
-% plot(sam3(1:1000,19), sam3(1:1000,20), '*');
-% xlim([0.5,1]);
-% text(0.51, 0.6, '$\mathbf{\bar{T}=665}$', 'color', 'r', 'fontsize', 12, 'Interpreter', 'latex');
-% title('v_0 constant, MTDCs do not feedback'), xlabel('p_m'), ylabel('v_0')
-% 
-% subplot(2,2,2), plot(sam4(1:1000,19), sam4(1:1000,20), '*'), xlim([0.5,1]), text(0.51, 0.6, '$\mathbf{\bar{T}=22700}$', 'color', 'r', 'fontsize', 12, 'Interpreter', 'latex'), title('v_0 change, MTDCs do not feedback'), xlabel('p_m'), ylabel('v_0')
-% 
-% subplot(2,2,3), plot(sam1(1:1000,19), sam1(1:1000,20), '*'), xlim([0.5,1]), text(0.51, 0.6, '$\mathbf{\bar{T}=483}$', 'color', 'r', 'fontsize', 12, 'Interpreter', 'latex'), title('v_0 constant, MTDCs feedback'), xlabel('p_m'), ylabel('v_0')
-% 
-% subplot(2,2,4), plot(sam2(1:1000,19), sam2(1:1000,20), '*'), xlim([0.5,1]), text(0.51, 0.6, '$\mathbf{\bar{T}=678}$', 'color', 'r', 'fontsize', 12, 'Interpreter', 'latex'), title('v_0 change, MTDCs feedback'), xlabel('p_m'), ylabel('v_0')
-% 
-% print('-depsc','fig_logi_random_mutation.eps')
+% ===========
+% test
+% ===========
+load tr
+clf;
+tr = tr;
+plot(tr(:,1), tr(:,2), 'r')
+%xlim([150, 350])
+hold on
+plot(tr(:,1), tr(:,3), 'b')
+plot(tr(:,1), tr(:,4), 'g')
+plot(tr(:,1), tr(:,5), 'k')
+% figure
+plot(tr(:,1), tr(:,7), 'm')
+xlabel('t','fontsize', 18);
+ylabel('cell population','fontsize',18)
+legend('SC', 'TAC', 'TDC', 'MC', 'Total')
 
 % % ===========
 % % script for 2 feedback, self-recovery, asym/asym
@@ -69,39 +36,16 @@
 % print('-depsc','fig_logi_selfrecover_aa.eps')
 % 
 % figure
-% h = plot(tr(:,1), tr(:,2), '-r','linewidth', 1.1)
-% xlim([100, 250])
+% xlim([0, 450])
 % hold on
-% plot(tr(:,1), tr(:,3), '-b','linewidth', 1.1)
-% plot(tr(:,1), tr(:,4), '-g','linewidth', 1.1)
-% plot(tr(:,1), tr(:,5), 'k','linewidth', 1.1)
-% plot(tr(:,1), tr(:,7), '-m','linewidth', 1.1)
+% plot(tr(:,1), tr(:,9), '-b','linewidth', 1.1)
+% plot(tr(:,1), tr(:,10), '-r','linewidth', 1.1)
 % xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'MC', 'total')
-% print('-depsc','fig_logi_selfrecover_aa_zoomin.eps')
-% 
-% figure
-% hl1 = line(tr(:,1),tr(:,9),'Color','b','linewidth', 1.1);
-% xlabel('t','fontsize', 18);
-% ylabel('p_0','fontsize',18)
-% legend('p_0')
+% ylabel('p_0, v_0','fontsize',18)
+% legend('p_0', 'v_0')
 % %ylim([0, 600])
 % %set(get(h,'axis'),'FontSize',24)
 % box on
-% hold on
-% ax1 = gca;
-% xlim([0, 450])
-% ax2 = axes('Position',get(ax1,'Position'),...
-%            'XAxisLocation','top',...
-%            'YAxisLocation','right',...
-%            'Color','none',...
-%            'XColor','k','YColor','k');
-% line(1000,0, 'Color','b','linewidth', 1.1);
-% hl2 = line(tr(:,1),tr(:,10),'Color','r','Parent',ax2,'linewidth', 1.1);
-% xlim([0, 450])
-% ylabel('v_0','fontsize',18)
-% legend('p_0', 'v_0')
 % print('-depsc','fig_logi_selfrecover_pvplot_aa.eps')
 
 % % ===========
@@ -245,9 +189,9 @@
 % print('-depsc','fig_logi_mutation_system2.eps')
 
 
-% ===========
-% script for pie
-% ===========
+% % ===========
+% % script for pie
+% % ===========
 % load pr2_sample_system1
 % sample = pr2_sample_system1;
 % counter = zeros(1,4);
@@ -256,17 +200,16 @@
 %         counter(4) = counter(4) + 1;
 %     end
 %     if ( sample(i,5) == 0 && sample(i,6) ~= 0 )
-%         counter(3) = counter(3) + 1;
+%         counter(1) = counter(1) + 1;
 %     end
 %     if ( sample(i,5) ~= 0 && sample(i,6) == 0 )
-%         counter(1) = counter(1) + 1;
+%         counter(3) = counter(3) + 1;
 %     end
 %     if ( sample(i,5) ~= 0 && sample(i,6) ~= 0)
 %         counter(2) = counter(2) + 1;
 %     end
 % end
-% subplot(1, 2, 2), pie(counter(1:3))
-% title('System 2')
+% subplot(1, 2, 1), pie(counter(1:3))
 % %colormap cool
 % %print('-depsc','fig_logi_mpie_system1.eps')
 % 
@@ -281,32 +224,28 @@
 %         counter(4) = counter(4) + 1;
 %     end
 %     if ( sample(i,5) == 0 && sample(i,6) ~= 0 )
-%         counter(3) = counter(3) + 1;
+%         counter(1) = counter(1) + 1;
 %     end
 %     if ( sample(i,5) ~= 0 && sample(i,6) == 0 )
-%         counter(1) = counter(1) + 1;
+%         counter(3) = counter(3) + 1;
 %     end
 %     if ( sample(i,5) ~= 0 && sample(i,6) ~= 0)
 %         counter(2) = counter(2) + 1;
 %     end
 % end
-% subplot(1, 2, 1), pie(counter(1:3))
+% subplot(1, 2, 2), pie(counter(1:3))
 % %colormap cool
-% title('System 1')
 % print('-depsc','fig_logi_mpie.eps')
 
 % % ===========
 % % script for stable solution
 % % ===========
-% load pr2_stable_aa06
+% load pr2_stable_ss06
 % clf;
-% tr = pr2_stable_aa06;
+% tr = pr2_stable_ss06;
 % plot(tr(:,1), tr(:,2), 'r','linewidth', 2)
 % xlim([0, 200])
 % ylim([0, 250])
-% partition(1,1) = mean(tr(50:200,2))
-% partition(1,2) = mean(tr(50:200,3))
-% partition(1,3) = mean(tr(50:200,4))
 % hold on
 % plot(tr(:,1), tr(:,3), 'b','linewidth', 2)
 % plot(tr(:,1), tr(:,4), 'g','linewidth', 2)
@@ -316,57 +255,7 @@
 % xlabel('t','fontsize', 18);
 % ylabel('cell population','fontsize',18)
 % legend('SC', 'TAC', 'TDC', 'Total')
-% print('-depsc','fig_logi_stable_aa06.eps')
-% 
-% load pr2_stable_aa08
-% clf;
-% tr = pr2_stable_aa08;
-% plot(tr(:,1), tr(:,2), 'r','linewidth', 2)
-% xlim([0, 200])
-% ylim([0, 250])
-% partition(2,1) = mean(tr(50:200,2))
-% partition(2,2) = mean(tr(50:200,3))
-% partition(2,3) = mean(tr(50:200,4))
-% hold on
-% plot(tr(:,1), tr(:,3), 'b','linewidth', 2)
-% plot(tr(:,1), tr(:,4), 'g','linewidth', 2)
-% %plot(tr(:,1), tr(:,5), 'k','linewidth', 2)
-% % figure
-% plot(tr(:,1), tr(:,7), 'm','linewidth', 2)
-% xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'Total')
-% print('-depsc','fig_logi_stable_aa08.eps')
-% 
-% load pr2_stable_aa10
-% clf;
-% tr = pr2_stable_aa10;
-% plot(tr(:,1), tr(:,2), 'r','linewidth', 2)
-% xlim([0, 200])
-% ylim([0, 250])
-% partition(3,1) = mean(tr(50:200,2))
-% partition(3,2) = mean(tr(50:200,3))
-% partition(3,3) = mean(tr(50:200,4))
-% hold on
-% plot(tr(:,1), tr(:,3), 'b','linewidth', 2)
-% plot(tr(:,1), tr(:,4), 'g','linewidth', 2)
-% %plot(tr(:,1), tr(:,5), 'k','linewidth', 2)
-% % figure
-% plot(tr(:,1), tr(:,7), 'm','linewidth', 2)
-% xlabel('t','fontsize', 18);
-% ylabel('cell population','fontsize',18)
-% legend('SC', 'TAC', 'TDC', 'Total')
-% print('-depsc','fig_logi_stable_aa10.eps')
-% 
-% figure
-% map(1, :) = [1, 0, 0];
-% map(2, :) = [0, 0, 1];
-% map(3, :) = [0, 1, 0];
-% colormap(map)
-% bar(partition)
-% legend('SC', 'TAC', 'TDC')
-% set(gca,'xticklabel',{'k=0.6','k=0.8','k=1'}, 'fontsize', 12)
-% print('-depsc','fig_logi_stable_aa_partition.eps')
+% print('-depsc','fig_logi_stable_ss06.eps')
 
 % % ===========
 % % plot the average MC vs pm
@@ -417,65 +306,30 @@
 % legend('feedback scheme II', 'feedback scheme III', 2)
 % %set(get(h,'axis'),'FontSize',24)
 % print('-depsc','fig_logi_pm.eps')
-
-% ===========
-% script pm ultra
-% ===========
-load pr2_p0v0_data2
-data = pr2_p0v0_data2
-ymap = 0.1:.1:3.009;
-xmap = 0.0:0.02:1.0001;
-[X,Y] = meshgrid(xmap, ymap);
-wg_barmap = reshape((data(:,6)+data(:,7))./(data(:,3)+data(:,4)+data(:,5)+data(:,6)+data(:,7)), length(ymap), length(xmap))
-wg_barmap2 = reshape((data(:,6))./(data(:,3)+data(:,4)+data(:,5)+data(:,6)+data(:,7)), length(ymap), length(xmap))
-xmap = 0.5:0.02:1.0001;
-[X,Y] = meshgrid(xmap, ymap);
-wg_barmap = wg_barmap(:, 26:51)
-wg_barmap2 = wg_barmap2(:, 26:51)
-pcolor(Y, X, wg_barmap)
-%surf(X, Y, wg_barmap)
-
-colorbar;
-xlabel('v_m','fontsize', 18);
-ylabel('p_m','fontsize',18);
-zlabel('ratio of mutant cell','fontsize',18);
-%xlim([0.5,1]);
-print('-depsc','fig_logi_pmvm.eps')
-
+% 
+% % ===========
+% % script pm ultra
+% % ===========
+% load pr2_p0v0_data2
+% data = pr2_p0v0_data2
+% ymap = 0.1:.1:3.009;
+% xmap = 0.0:0.02:1.0001;
+% [X,Y] = meshgrid(xmap, ymap);
+% wg_barmap = reshape((data(:,6)+data(:,7))./(data(:,3)+data(:,4)+data(:,5)+data(:,6)+data(:,7)), length(ymap), length(xmap))
+% surf(X, Y, wg_barmap)
+% xlabel('p_m','fontsize', 18);
+% ylabel('v_m','fontsize',18);
+% zlabel('ratio of mutant cell','fontsize',18);
+% xlim([0.5,1]);
+% %print('-depsc','fig_logi_pmvm.eps')
 % figure
-% plot(xmap, wg_barmap2(7, :),'g', 'linewidth', 2)
+% plot(xmap, wg_barmap(10, :),'linewidth', 2)
 % hold on
-% plot(xmap, wg_barmap2(10, :),'linewidth', 2)
-% plot(xmap, wg_barmap2(15, :),'r', 'linewidth', 2)
+% plot(xmap, wg_barmap(7, :),'g', 'linewidth', 2)
+% plot(xmap, wg_barmap(15, :),'r', 'linewidth', 2)
 % %plot(xmap, wg_barmap(25, :),'r', 'linewidth', 2)
 % xlim([0.5,1]);
 % legend('v_m = 0.7', 'v_m=1', 'v_m=1.5','location', 'northwest')
-% 
 % xlabel('p_m','fontsize', 18);
-% ylabel('ratio of cancer stem cell','fontsize',18);
+% ylabel('ratio of mutant cell','fontsize',18);
 % print('-depsc','fig_logi_pmvm_slicev.eps')
-figure
-
-for index = 1:length(ymap)
-    pmax(index) = 0;
-    prob(index) = 0;
-    for temp_j = 1:length(xmap)
-        if ( wg_barmap(index, temp_j) > pmax(index) )
-            pmax(index) = wg_barmap(index, temp_j)
-            %pmax(index) = xmap(temp_j)
-            prob(index) = xmap(temp_j);
-            csc(index) = wg_barmap2(index, temp_j)
-            ctt(index) = wg_barmap(index, temp_j)
-        end
-    end
-end
-hold on
-%plot(ymap, prob, 'o', 'markersize', 10, 'LineWidth',2, 'MarkerEdgeColor', 'k', 'MarkerFaceColor',[.49 1 .63])
-box on
-plot(ymap, csc,'ks', 'LineWidth',2, 'markersize', 12, 'markerfacecolor', 'r')
-% plot(ymap, ctt,'ks', 'LineWidth',2, 'markersize', 12, 'markerfacecolor', 'g')
-
-xlabel('v_m','fontsize', 18);
-ylabel('ratio of cancer stem cell','fontsize',18);
-print('-depsc','fig_logi_pmvm_csc.eps')
-    

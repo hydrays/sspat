cc58c3e6119084c3a6c4d41658db4af9f36f6436
hydrays <hydrays@gmail.com>
2012-03-14 05:52:34 +0000

    logichem: mar2012pmvm: 2feedback+asym+MTDCfeedback

--- problem/logichem/setting.f90
+++ problem/logichem/setting.f90
@@ -5,7 +5,7 @@
 !!$ (1,  2,  3,  4,  5)
 !!$  SC  TA  TC  MC  TMC
 
-!!$ >>> NReac = 8
+!!$ >>> NReac = 18
 
 !!$  1: SC -> 2 SC
 !!$  2: SC -> SC + TA
@@ -14,44 +14,48 @@
 !!$  5: TA -> TA + TC
 !!$  6: TA -> 2 TC
 !!$  7: MC -> 2 MC
-!!$  8: MC -> 2 TMC
-!!$  9: TC -> 0
-!!$  10: TMC -> 0
-!!$  11: SC -> 0
-!!$  12: TA -> 0
-!!$  13: TC -> 0
-!!$  14: MC -> 0
-!!$  15: TMC -> 0
-!!$  16: SC -> MC
-!!$  17: TAC -> MC
+!!$  8: MC -> MC + TMC
+!!$  9: MC -> 2 TMC
+!!$  10: TC -> 0
+!!$  11: TMC -> 0
+!!$  12: SC -> 0
+!!$  13: TA -> 0
+!!$  14: TC -> 0
+!!$  15: MC -> 0
+!!$  16: TMC -> 0
+!!$  17: SC -> MC
+!!$  18: TAC -> MC
 
 module chem_data  
   use nrtype
   implicit none
-  integer(I4B) :: NSample = 1
+  integer(I4B) :: NSample = 1000
   integer(I4B), parameter :: NSpec=5
-  integer(I4B), parameter :: NReac=17
-  real(kind=8) ap, p0, p1, v0, q1, q2, q3, vmut
+  integer(I4B), parameter :: NReac=18
+  real(kind=8) ap, p0, p1, v0
+  real(kind=8) pm, vm
   real(kind=8) k1, k2, v0max, v0min
+  real(kind=8) q1, q2, q3
   real(kind=8) qq1, qq2, qq3
-  real(kind=8), parameter :: L = 1000
+  real(kind=8) qm1, qm2, qm3
+  real(kind=8), parameter :: L = 200
   real(kind=8), parameter :: mu = 0.0
 
-  integer(I4B), parameter :: Xinit(NSpec)=(/ &
-       10, & !SC
-       0, & !TA
-       0, & !TC
-       00, & !MC
-       00 & !TMC
-       /)
-!!$
 !!$  integer(I4B), parameter :: Xinit(NSpec)=(/ &
-!!$       30, & !SC
-!!$       50, & !TA
-!!$       120, & !TC
+!!$       10, & !SC
+!!$       0, & !TA
+!!$       0, & !TC
 !!$       00, & !MC
 !!$       00 & !TMC
 !!$       /)
+
+  integer(I4B), parameter :: Xinit(NSpec)=(/ &
+       30, & !SC
+       50, & !TA
+       120, & !TC
+       00, & !MC
+       00 & !TMC
+       /)
 
 !!$  integer(I4B), parameter :: Xinit(NSpec)=(/ &
 !!$       100, & !SC
@@ -70,19 +74,18 @@ module chem_data
        (/00, 01, 00, 00, 00/), & !4
        (/00, 00, 01, 00, 00/), & !5
        (/00, -1, 02, 00, 00/), & !6
-!!       (/00, 00, 00, 00, 01/), & !7
-!!       (/00, 00, 00, 01, 00/), & !8
        (/00, 00, 00, 01, 00/), & !7
-       (/00, 00, 00, -1, 02/), & !8
-       (/00, 00, -1, 00, 00/), & !9
-       (/00, 00, 00, 00, -1/), & !10
-       (/-1, 00, 00, 00, 00/), & !11
-       (/00, -1, 00, 00, 00/), & !12
-       (/00, 00, -1, 00, 00/), & !13
-       (/00, 00, 00, -1, 00/), & !14
-       (/00, 00, 00, 00, -1/), & !15
-       (/-1, 00, 00, 00, 01/), & !16
-       (/00, -1, 00, 01, 00/) & !17
+       (/00, 00, 00, 00, 01/), & !8
+       (/00, 00, 00, -1, 02/), & !9
+       (/00, 00, -1, 00, 00/), & !10
+       (/00, 00, 00, 00, -1/), & !11
+       (/-1, 00, 00, 00, 00/), & !12
+       (/00, -1, 00, 00, 00/), & !13
+       (/00, 00, -1, 00, 00/), & !14
+       (/00, 00, 00, -1, 00/), & !15
+       (/00, 00, 00, 00, -1/), & !16
+       (/-1, 00, 00, 01, 00/), & !17
+       (/00, -1, 00, 01, 00/) & !18
        /), shape = (/NSpec, NReac/) &
        )
 
@@ -92,15 +95,20 @@ contains
     real(kind=8), intent(in) :: x(NSpec)
     real(kind=8), intent(out) :: a(NReac)
     real(kind=8), intent(in) :: pm
+    real(kind=8) TGFbeta 
+
+    TGFbeta = x(3) + x(5)
+    !TGFbeta = x(3)
+
     v0max = 3.0
     v0min = 0.5
     k1 = 1.0
     k2 = v0max/v0min - 1.0
     p1 = 0.4
-    p0 = 1.0/(1.01 + k1*(x(3)+x(5))/L)
-    v0 = v0max/(1.0 + k2*(x(3)+x(5))/L)
-    !v0 = 0.65
+    p0 = 1.0/(1.01 + k1*TGFbeta/L)
+    v0 = v0max/(1.0 + k2*TGFbeta/L)
 !!$
+
     if ( p0 .le. 0.5 ) then
        q2 = 2.0*p0
     else
@@ -121,6 +129,18 @@ contains
 !!$    qq2 = 0.0
 !!$    qq3 = 1.0 - qq1 - qq2
 
+    if ( pm .le. 0.5 ) then
+       qm2 = 2.0*pm
+    else
+       qm2 = -2.0*(pm - 1.0)
+    end if
+    qm1 = pm - qm2*0.5
+    qm3 = 1.0 - qm1 - qm2
+
+!!$    qm1 = pm
+!!$    qm2 = 0.0
+!!$    qm3 = 1.0 - qm1
+
     if (sum(x) > L) then
        ap = 0.1*(sum(x) - L)
     else
@@ -134,25 +154,22 @@ contains
     a(5) = qq2*x(2)
     a(6) = qq3*x(2)
 
-    a(7) = vmut*x(4)*pm
-    a(8) = vmut*x(4)*(1.0-pm)
+    a(7) = qm1*vm*x(4)
+    a(8) = qm2*vm*x(4)
+    a(9) = qm3*vm*x(4)
+    
+    a(10) = 0.2*x(3)
+    a(11) = 0.2*x(5)
 
-    !!a(7) = v0*x(5)
-    !!a(8) = x(4)
+    a(12) = ap*x(1)
+    a(13) = ap*x(2)
+    a(14) = ap*x(3)
 
-    a(9) = 0.2*x(3)
-    a(10) = 0.2*x(5)
-    !!a(10) = 0.0*x(5)
+    a(15) = ap*x(4)
+    a(16) = ap*x(5)
 
-    a(11) = ap*x(1)
-    a(12) = ap*x(2)
-    a(13) = ap*x(3)
-
-    a(14) = ap*x(4)
-    a(15) = ap*x(5)
-
-    a(16) = mu*v0*x(1)
-    a(17) = mu*x(2)
+    a(17) = mu*v0*x(1)
+    a(18) = mu*x(2)
 
   end subroutine getrate
 

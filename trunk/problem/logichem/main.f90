bb97dfce6fb490b0e6500a7a90e674d4d6f7c91a
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-03-20 22:15:54 +0000

    logichem: mar2012pmvm: 1fpmvm0910_B

--- trunk/problem/logichem/main.f90
+++ trunk/problem/logichem/main.f90
@@ -19,8 +19,8 @@ program ssa
   call ran_seed(sequence=1234)
   te = 1000.0
   !te = huge(1.0)
-  pm = 0.8
-  vm = 0.9
+  pm = 0.9
+  vm = 1.0
 
 !  do pm = 0.5, 1.0001, 0.02
 !  do vm = 0.1, 3.01, 0.03

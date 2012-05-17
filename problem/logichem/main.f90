c02a3da45e5587be9734d8a7f591de590d2afd5b
hydrays <hydrays@gmail.com>
2012-05-17 18:32:39 +0000

    logichem: revert to version 139.

--- problem/logichem/main.f90
+++ problem/logichem/main.f90
@@ -15,16 +15,16 @@ program ssa
   integer(I4B) i, j, k, index
   real(kind=8) N_mutation
   real(kind=8) xbar(NSpec), tbar, xbar_counter
+  real(kind=8) x1_peak, t1_peak
+  real(kind=8) x2_peak, t2_peak
 
-  call ran_seed(sequence=12342)
-  te = 1000.0
-  !te = huge(1.0)
-!  pm = 1.0
-!  vm = 2.9
+  call ran_seed(sequence=1234)
+  te = 400.0
+  pm = 1.0
+  vm = 1.0
 
-  do pm = 0.0, 1.0001, 0.02
-  do vm = 0.1, 3.01, 0.05
-!  xbar = 0.0
+  !do pm = 0.5, 1.0001, 0.01
+  !do vm = 0.1, 3.01, 0.01
   takeover_counter = 0.0
   nottakeover_counter = 0.0
   coexist_counter = 0.0
@@ -39,8 +39,12 @@ program ssa
      takeover_flag = 0.0
      nottakeover_flag = 0.0
      max_SCnum = 0.0
+     x1_peak = 0.0
+     t1_peak = 0.0
+     x2_peak = 0.0
+     t2_peak = 0.0
      do while(t < te)
-        call getrate(x, a, pm)
+        call getrate(x, a)
         cuma = a
         do j=2, NReac
            cuma(j) = cuma(j-1) + a(j)
@@ -55,61 +59,65 @@ program ssa
            j = j+1
         end do
 
-!!$        if ( x(4) .ne. 0) then
-!!$           mtime = mtime + delta_t
-!!$        end if
-
         x = x + nu(:, j)
-!!$        if (j.eq.16) then
-!!$           print *, x
-!!$           read(*,*)
-!!$        end if
+
         call checkx(x, is_nag)
         if (is_nag .eq. 1) then
            print *, 'nag'
            pause
         end if
         
-!!$        if(t > tp) then
-!!$           write (*, '(F10.2, 10F8.2)'), t, x, sum(x)
-!!$           tp =  tp + 1.0
-!!$        end if
+        if(t > tp) then
+           write (*, '(F10.2, 10F8.2)'), t, x, sum(x), p1, v1
+           tp =  tp + 1.0
+        end if
 
         if(t > td) then
            if ( x(4) .eq. 0 ) then
-              x(4) = 1
+              x(4) = 10
            end if
            td =  td + 400000.0
         end if
 
-        if ( x(1) .gt. max_SCnum ) then
-           max_SCnum = x(1)
-        end if
-        if ( x(1)+x(2)+x(3).eq.0 ) then
-           takeover_counter = takeover_counter + 1.0
-           takeover_flag = 1.0
-           exit
+        !if (x(1) .gt. max_SCnum ) then
+        !   max_SCnum = x(1)
+        !end if
+        
+        if ( t>201 .and. x1_peak < x(1) ) then
+           x1_peak = x(1)
+           t1_peak = t
         end if
 
-        if ( t > 201.0 .and. x(4)+x(5).eq.0 ) then
-           nottakeover_counter = nottakeover_counter + 1.0
-           nottakeover_flag = 1.0
-           exit
+        if ( t>201 .and. x2_peak < x(4) ) then
+           x2_peak = x(4)
+           t2_peak = t
         end if
+
+
+!!$        if ( x(1)+x(2)+x(3).eq.0 ) then
+!!$           takeover_counter = takeover_counter + 1.0
+!!$           takeover_flag = 1.0
+!!$           exit
+!!$        end if
+!!$
+!!$        if ( t > 201.0 .and. x(4)+x(5).eq.0 ) then
+!!$           nottakeover_counter = nottakeover_counter + 1.0
+!!$           nottakeover_flag = 1.0
+!!$           exit
+!!$        end if
 
      end do
-!     xbar = xbar + x
      if ( takeover_flag .eq. 0.0 .and. nottakeover_flag .eq. 0.0 ) then
         coexist_counter = coexist_counter + 1.0
      end if
      average_max_SCnum = average_max_SCnum + max_SCnum
-     !write (*, '(F10.2, 10F8.2)'), t, x, sum(x)
+!     write (*, '(F10.2, 10F8.2)'), t1_peak, x1_peak, t2_peak, x2_peak 
   end do
   average_max_SCnum = average_max_SCnum/real(NSample)
-  write (*, '(10F12.2)'), pm, vm, takeover_counter, nottakeover_counter, &
-       coexist_counter, average_max_SCnum 
-  end do 
-  end do
+!!$  write (*, '(10F12.2)'), pm, vm, takeover_counter, nottakeover_counter, &
+!!$       coexist_counter, average_max_SCnum 
+!!$  end do 
+!!$  end do
 end program ssa
 
 subroutine checkx(x, is_nag)
--- problem/logichem/setting.f90
+++ problem/logichem/setting.f90
@@ -29,16 +29,17 @@
 module chem_data  
   use nrtype
   implicit none
-  integer(I4B) :: NSample = 200
+  integer(I4B) :: NSample = 100
   integer(I4B), parameter :: NSpec=5
   integer(I4B), parameter :: NReac=18
-  real(kind=8) ap, p0, p1, v0
-  real(kind=8) pm, vm
-  real(kind=8) k1, k2, v0max, v0min
+  real(kind=8) ap, p0, v0
+  real(kind=8) pm, vm, p1, v1
+  real(kind=8) ksc1, ksc2, ktac1, ktac2, kmc1, kmc2
+  real(kind=8) v0max, v0min, v1min, v1max, vmmax, vmmin
   real(kind=8) q1, q2, q3
   real(kind=8) qq1, qq2, qq3
   real(kind=8) qm1, qm2, qm3
-  real(kind=8), parameter :: L = 20000
+  real(kind=8), parameter :: L = 200
   real(kind=8), parameter :: mu = 0.0
 
 !!$  integer(I4B), parameter :: Xinit(NSpec)=(/ &
@@ -90,24 +91,40 @@ module chem_data
        )
 
 contains
-  subroutine getrate(x, a, pm)
+  subroutine getrate(x, a)
     implicit none
     real(kind=8), intent(in) :: x(NSpec)
     real(kind=8), intent(out) :: a(NReac)
-    real(kind=8), intent(in) :: pm
     real(kind=8) TGFbeta 
 
-    TGFbeta = x(3) + x(5)
-    !TGFbeta = x(3)
+    !TGFbeta = x(3) + x(5)
+    TGFbeta = x(3)
 
     v0max = 3.0
     v0min = 0.5
-    k1 = 1.0
-    k2 = v0max/v0min - 1.0
-    p1 = 0.4
-    p0 = 1.0/(1.01 + k1*TGFbeta/L)
-    v0 = v0max/(1.0 + k2*TGFbeta/L)
-!!$
+    ksc1 = 1.0
+    ksc2 = v0max/v0min - 1.0
+    p0 = 1.0/(1.01 + ksc1*TGFbeta/L)
+    v0 = v0max/(1.0 + ksc2*TGFbeta/L)
+    !v0 = 0.65
+
+    v1max = 1.3
+    v1min = 0.7
+    ktac1 = 0.8
+    ktac2 = v1max/v1min - 1.0
+    p1 = 0.6/(1.01 + ktac1*TGFbeta/L)
+    v1 = v1max/(1.0 + ktac2*TGFbeta/L)    
+    !p1 = 0.4
+    !v1 = 1.0
+
+    !vmmax = 3.0
+    !vmmin = 0.5
+    !kmc1 = 1.0
+    !kmc2 = v0max/v0min - 1.0
+    !pm = 1.0/(1.01 + kmc1*TGFbeta/L)
+    !vm = vmmax/(1.0 + kmc2*TGFbeta/L)    
+    !pm = 0.4
+    !vm = 1.0
 
     if ( p0 .le. 0.5 ) then
        q2 = 2.0*p0
@@ -121,14 +138,19 @@ contains
 !!$    q2 = 0.0
 !!$    q3 = 1.0 - q1
 
-    qq1 = 0.0
-    qq2 = 2.0*p1
+    if ( p1 .le. 0.5 ) then
+       qq2 = 2.0*p1
+    else
+       qq2 = -2.0*(p1 - 1.0)
+    end if
+    qq1 = p1 - qq2*0.5
     qq3 = 1.0 - qq1 - qq2
-!!$
+
+
 !!$    qq1 = p1
 !!$    qq2 = 0.0
 !!$    qq3 = 1.0 - qq1 - qq2
-
+!!$
     if ( pm .le. 0.5 ) then
        qm2 = 2.0*pm
     else
@@ -142,17 +164,20 @@ contains
 !!$    qm3 = 1.0 - qm1
 
     if (sum(x) > L) then
-       ap = 0.001*(sum(x) - L)
+       ap = 0.1*(sum(x) - L)
     else
        ap = 0.0
     end if
+
+!!$    ap = 0.2*sum(x)
+
     a(1) = q1*v0*x(1)
     a(2) = q2*v0*x(1)
     a(3) = q3*v0*x(1)
 
-    a(4) = qq1*x(2)
-    a(5) = qq2*x(2)
-    a(6) = qq3*x(2)
+    a(4) = qq1*v1*x(2)
+    a(5) = qq2*v1*x(2)
+    a(6) = qq3*v1*x(2)
 
     a(7) = qm1*vm*x(4)
     a(8) = qm2*vm*x(4)

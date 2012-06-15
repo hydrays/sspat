869c639258831f4dc6d864621995ed8b6a1de867
hydrays <hydrays@gmail.com>
2012-06-15 17:58:28 +0000

    logichem: preparing to role back to generate a trace.

--- problem/logichem/main.f90
+++ problem/logichem/main.f90
@@ -8,129 +8,82 @@ program ssa
   real(kind=8) te
   real(kind=8) a(NReac), cuma(NReac), u
   real(kind=8) delta_t, t, tp, td, mtime
-  real(kind=8) takeover_counter, nottakeover_counter, coexist_counter
-  real(kind=8) takeover_flag, nottakeover_flag
-  real(kind=8) max_SCnum, average_max_SCnum
   integer(I4B) is_nag, tac_mflag, sc_mflag
   integer(I4B) i, j, k, index
-  real(kind=8) N_mutation
   real(kind=8) xbar(NSpec), tbar, xbar_counter
-  real(kind=8) x1_peak, t1_peak
-  real(kind=8) x2_peak, t2_peak
+  integer(I4B) FID1, CeilN
 
   call ran_seed(sequence=12342)
+
   te = 400.0
   pm = 1.0
   vm = 1.0
 
-  !do ksc1 = 0.7, 0.9, 0.001
-  !do vm = 0.1, 3.01, 0.01
-  takeover_counter = 0.0
-  nottakeover_counter = 0.0
-  coexist_counter = 0.0
-  average_max_SCnum = 0.0
-  do index = 1.0, NSample
-     x = xinit
-     t = 0.0
-     tp = 0.0
-     td = 150.0
-     tbar = 0.0
-     xbar = 0.0
-     xbar_counter = 0.0
-     takeover_flag = 0.0
-     nottakeover_flag = 0.0
-     max_SCnum = 0.0
-     x1_peak = 0.0
-     t1_peak = 0.0
-     x2_peak = 0.0
-     t2_peak = 0.0
-     do while(t < te)
-        call getrate(x, a)
-        cuma = a
-        do j=2, NReac
-           cuma(j) = cuma(j-1) + a(j)
-        end do
-        call expdev(delta_t)
-        delta_t = delta_t/cuma(NReac)
-        t = t + delta_t
-        call ran2(u)
-        u = cuma(NReac)*u
-        j = 1
-        do while (cuma(j) .le. u)
-           j = j+1
-        end do
+  Fid1 = 10
 
-        x = x + nu(:, j)
+  do CeilN = 111,120
+     call prepare_output_files("mute_ed", CeilN, Fid1)
+     L = 20000*(CeilN-110)
+     Xinit = (/2, 3, 15, 0, 0/)
+     Xinit = Xinit*(CeilN-110)*1000
+     print *, CeilN, L, Xinit
+     do index = 1.0, NSample
+        x = xinit
+        t = 0.0
+        tp = 0.0
+        td = 150.0
+        tbar = 0.0
+        xbar = 0.0
+        xbar_counter = 0.0
+        do while(t < te)
+              call getrate(x, a)
+              cuma = a
+              do j=2, NReac
+                 cuma(j) = cuma(j-1) + a(j)
+              end do
+              call expdev(delta_t)
+              delta_t = delta_t/cuma(NReac)
+              t = t + delta_t
+              call ran2(u)
+              u = cuma(NReac)*u
+              j = 1
+              do while (cuma(j) .le. u)
+                 j = j+1
+              end do
 
-        call checkx(x, is_nag)
-        if (is_nag .eq. 1) then
-           print *, 'nag'
-           pause
-        end if
-        
-!!$        if(t > tp) then
-!!$           write (*, '(F10.2, 10F10.2)'), t, x, sum(x), p0, v0
-!!$           tp =  tp + 1.0
-!!$        end if
+              x = x + nu(:, j)
 
-!!$        if(t > tp) then
-!!$           xbar = xbar + x;
-!!$           xbar_counter =  xbar_counter + 1.0
-!!$           tp =  tp + 1.0
-!!$        end if
+              call checkx(x, is_nag)
+              if (is_nag .eq. 1) then
+                 print *, 'nag'
+                 pause
+              end if
 
-        if(t > td) then
-           if ( x(4) .eq. 0 ) then
-              x(4) = 1
-              !x(3) = int(x(3)/4.0)
-              !x(2) = int(x(2)/4.0)
-              !x(1) = int(x(1)/4.0)
+!!$           if(t > tp) then
+!!$              !write (*, '(F10.2, 10F10.2)'), t, x, sum(x), p0, v0
+!!$              write (Fid1, '(F10.2, 10F10.2)'), t, x, sum(x), p0, v0
+!!$              tp =  tp + 1.0
+!!$           end if
+
+           if(t > td) then
+              if ( x(4) .eq. 0 .and. sum(x).ne.0 ) then
+                 x(4) = 1
+                 !x(3) = int(x(3)/4.0)
+                 !x(2) = int(x(2)/4.0)
+                 !x(1) = int(x(1)/4.0)
+              end if
+              td =  td + 1500.0
            end if
-           td =  td + 1500.0
-        end if
 
-        !if (x(1) .gt. max_SCnum ) then
-        !   max_SCnum = x(1)
-        !end if
-        
-!!$        if ( t>201 .and. x1_peak < x(1) ) then
-!!$           x1_peak = x(1)
-!!$           t1_peak = t
-!!$        end if
-!!$
-!!$        if ( t>201 .and. x2_peak < x(4) ) then
-!!$           x2_peak = x(4)
-!!$           t2_peak = t
-!!$        end if
-
-
-!!$        if ( x(1)+x(2)+x(3).eq.0 ) then
-!!$           takeover_counter = takeover_counter + 1.0
-!!$           takeover_flag = 1.0
-!!$           exit
-!!$        end if
-!!$
-!!$        if ( t > 201.0 .and. x(4)+x(5).eq.0 ) then
-!!$           nottakeover_counter = nottakeover_counter + 1.0
-!!$           nottakeover_flag = 1.0
-!!$           exit
-!!$        end if
-
+           if (sum(x).eq.0) then
+              exit
+           end if
+        end do
+        !write (*, '(I10, 10F10.2)'), index, x
+        write (Fid1, '(I10, 10F10.2)'), index, x
      end do
-     if ( takeover_flag .eq. 0.0 .and. nottakeover_flag .eq. 0.0 ) then
-        coexist_counter = coexist_counter + 1.0
-     end if
-!     average_max_SCnum = average_max_SCnum + max_SCnum
-!     write (*, '(F10.2, 10F10.2)'), t1_peak, x1_peak, t2_peak, x2_peak 
-     write (*, '(I10, 10F10.2)'), index, x
-!     xbar = xbar/xbar_counter
+     close(Fid1)
   end do
-!  average_max_SCnum = average_max_SCnum/real(NSample)
-!!$  write (*, '(10F12.2)'), pm, vm, takeover_counter, nottakeover_counter, &
-!!$       coexist_counter, average_max_SCnum
-!  write (*, '(F10.4, 10F10.4)'), ksc1, xbar 
-!  end do 
-!!$  end do
 end program ssa
 
 subroutine checkx(x, is_nag)

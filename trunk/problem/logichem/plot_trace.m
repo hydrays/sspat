72fcba8e8bd5c2c051cb13bb6023c6ffe4d0c37f
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-08-13 03:38:31 +0000

    logichem: Rolled back to r140.

--- trunk/problem/logichem/plot_trace.m
+++ trunk/problem/logichem/plot_trace.m
@@ -1,22 +1,40 @@
 % ===========
 % test
 % ===========
+%figure
 load tr
 clf;
 tr = tr;
 plot(tr(:,1), tr(:,2), '-r')
-%xlim([150, 350])
+xlim([0, 300])
 hold on
 plot(tr(:,1), tr(:,3), '-b')
 plot(tr(:,1), tr(:,4), '-g')
 plot(tr(:,1), tr(:,5), '-k')
-% figure
 plot(tr(:,1), tr(:,7), '-m')
 xlabel('t','fontsize', 18);
 ylabel('cell population','fontsize',18)
 legend('SC', 'TAC', 'TDC', 'MC', 'Total')
+% print('-depsc','tac_pm1vm1_6.eps')
 % 
 % figure
+% plot(tr(:,1), tr(:,8), '-b')
+
+% %figure
+% load tr2
+% %clf;
+% tr = tr2;
+% %plot(tr(:,1), tr(:,2), '.k')
+% xlim([150, 250])
+% hold on
+% %plot(tr(:,1), tr(:,3), '.k')
+% %plot(tr(:,1), tr(:,4), '-k')
+% %plot(tr(:,1), tr(:,5), '-k')
+% %plot(tr(:,1), tr(:,7), '-m')
+% xlabel('t','fontsize', 18);
+% ylabel('cell population','fontsize',18)
+% legend('SC', 'TAC', 'TDC', 'MC', 'Total')
+% % print('-depsc','tac_pm1vm1_6.eps')
 
 % % ===========
 % % test
@@ -273,9 +291,9 @@ legend('SC', 'TAC', 'TDC', 'MC', 'Total')
 % % ===========
 % % script for stable solution
 % % ===========
-% load pr2_stable_ss10
+% load pr2_stable_ss06
 % clf;
-% tr = pr2_stable_ss10;
+% tr = pr2_stable_ss06;
 % plot(tr(:,1), tr(:,2), 'r','linewidth', 2)
 % xlim([0, 200])
 % ylim([0, 250])
@@ -288,7 +306,7 @@ legend('SC', 'TAC', 'TDC', 'MC', 'Total')
 % xlabel('t','fontsize', 18);
 % ylabel('cell population','fontsize',18)
 % legend('SC', 'TAC', 'TDC', 'Total')
-% print('-depsc','fig_logi_stable_ss10.eps')
+% print('-depsc','fig_logi_stable_ss06.eps')
 
 % % ===========
 % % plot the average MC vs pm

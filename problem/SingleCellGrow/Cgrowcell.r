f65bad6569e28cf2bbb9103b9087fb046f59bad3
hydrays <hydrays@gmail.com>
2012-09-17 06:37:20 +0000

    SingleCellGrow: C lib for simulator1. Reduce time from 60s to 1s.

--- problem/SingleCellGrow/Cgrowcell.r
+++ problem/SingleCellGrow/Cgrowcell.r
@@ -1,10 +1,11 @@
-source('simulator1.r')
+dyn.load('simu1.so')
+source('Csimulator1.r')
 
-for (j in seq(5)){
+for (j in seq(7)){
   N <- 100
-  m <- c(0, 50, 100, 200, 800)
-  T <- 14
-  x <- c(10, m[j])
+  m <- c(0, 50, 100, 200, 800, 2000, 10000)
+  T <- 30
+  x <- c(100, m[j])
   r <- seq(T+1)
   r[1] <- x[1]/sum(x)
 
@@ -16,23 +17,14 @@ for (j in seq(5)){
   u <- 0.3
 
   for (i in seq(T)){
-    tempx <- c(0, 0)
-    for (k in seq(x[1])){
-      y <- simulator1(1, l1, l2, d1, d2, u, 1)
-      tempx <- tempx + y
-    }
-    for (k in seq(x[2])){
-      y <- simulator1(1, l1, l2, d1, d2, u, 0)
-      tempx <- tempx + y
-    }
-    x <- tempx
+    x <- Csimulator1(1, l1, l2, d1, d2, u, x)
     cat(i, x, r[i], '\n')
     r[i+1] <- x[1]/sum(x) 
   }
 
   if (j==1) {
     plot(0:T, r, type="o", pch=1, main="ratio vs time",
-         xlim = c(0, 14),
+         xlim = c(0, T),
          ylim = c(0, 1),
          xlab="time (day)", ylab="ratio (good/total)")
     abline(h=c(((l1-d1)-(l2-d2)-u)/((l1-d1)-(l2-d2))))
@@ -47,3 +39,6 @@ text(8, 0.75, adj=0, "r1=1, d1=0.4")
 text(8, 0.7, adj=0, "r2=0.6, d2=0.3")
 text(8, 0.65, adj=0, "p=0.3")
 text(8, 0.6, adj=0, "w is changing")
+
+dev.copy(jpeg, "T40.jpg")
+dev.off()

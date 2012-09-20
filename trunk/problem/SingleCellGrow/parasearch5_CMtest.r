646188c28cbab05be9d35a6ab07511877929544a
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-09-20 03:01:37 +0000

    SingleCellGrow: use adk test

--- trunk/problem/SingleCellGrow/parasearch5_CMtest.r
+++ trunk/problem/SingleCellGrow/parasearch5_CMtest.r
@@ -6,58 +6,58 @@ data7 <- read.csv('data/8dayslow.csv')
 mcell <- as.numeric(data7[8,2:(ncol(data7[1,])-3)])
 mcell[is.na(mcell)] <- 0
 
-Nsample = 1000
-T = 8
-x <- seq(Nsample)
-d <- matrix(ncol=101, nrow=101)
-pvalue <- matrix(ncol=101, nrow=101)
-i <- 1
-for ( r2 in seq(1, 2, by=0.02) ){
-    j <- 1
-      for ( d2 in seq(0.5, 2, by=0.02) ){
-              for (k in seq(Nsample)){
-                y <- Csimulator1(T, 0, r2, 0, d2, 0, 0)
-                x[k] <- sum(y)
-		}
-              #dis <- cvmts.test(mcell, x)
-              pvalue[i,j] <- dis$p.value
-              d[i,j] <- dis$statistic
-              cat(c(d[i,j], pvalue[i,j], r1, d1, i, j),'\n')
-            j <- j+1
-        }
-	i <- i+1
-}
+## Nsample = 1000
+## T = 8
+## x <- seq(Nsample)
+## d <- matrix(ncol=101, nrow=101)
+## pvalue <- matrix(ncol=101, nrow=101)
+## i <- 1
+## for ( r2 in seq(1, 2, by=0.02) ){
+##     j <- 1
+##       for ( d2 in seq(0.5, 2, by=0.02) ){
+##               for (k in seq(Nsample)){
+##                 y <- Csimulator1(T, 0, r2, 0, d2, 0, 0)
+##                 x[k] <- sum(y)
+## 		}
+##               #dis <- cvmts.test(mcell, x)
+##               pvalue[i,j] <- dis$p.value
+##               d[i,j] <- dis$statistic
+##               cat(c(d[i,j], pvalue[i,j], r1, d1, i, j),'\n')
+##             j <- j+1
+##         }
+## 	i <- i+1
+## }
 
 
 ## -----------------------------------
 ## Plot the contour
 ## -----------------------------------
 
-## filled.contour(x = seq(0, 1.4, length.out=71), 
-## 		 y = seq(0, 1.4, length.out=71), 
-## 		 d[1:71,1:71], 
-## 		 color=terrain.colors,
-## 		 plot.title = title(main = "KS-distance between ECDFs [Good Cells]",
-## 		 xlab = "proliferation rate", 
-## 		 ylab = "death rate"),
-## 		 asp = 1, 
-## 		 plot.axes={ axis(1); axis(2); points(0.84,0.44,pch=17) },
-## 		 level=c(0, 0.12, 0.15, 0.2, 0.3, 0.4, 0.5, 0.7, 1))
+filled.contour(x = seq(0, 1.4, length.out=71), 
+		 y = seq(0, 1.4, length.out=71), 
+		 d[1:71,1:71], 
+		 color=terrain.colors,
+		 plot.title = title(main = "KS-distance between ECDFs [Good Cells]",
+		 xlab = "proliferation rate", 
+		 ylab = "death rate"),
+		 asp = 1, 
+		 plot.axes={ axis(1); axis(2); points(0.84,0.44,pch=17) },
+		 level=c(0, 0.12, 0.15, 0.2, 0.3, 0.4, 0.5, 0.7, 1))
 
-## mtext(paste("@", date()), side=1, line=4, adj=1.04, cex=.66)
+mtext(paste("@", date()), side=1, line=4, adj=1.04, cex=.66)
 
-## text(0.4, 1.3, "Grid search for best fit using one-species model.")
-## text(0.4, 1.25, "The best fit is located at")
-## text(0.4, 1.2, "r1 = 0.84, d1 = 0.44 (triangle)")
+text(0.4, 1.3, "Grid search for best fit using one-species model.")
+text(0.4, 1.25, "The best fit is located at")
+text(0.4, 1.2, "r1 = 0.84, d1 = 0.44 (triangle)")
 
-## if( require(png) ) {
-##   img <- readPNG("bear.png")
-##   my.symbols( 1.04, -0.36, ms.image, MoreArgs=list(img=img),
-##              inches=0.2, symb.plots=TRUE, add=TRUE)
-## }
+if( require(png) ) {
+  img <- readPNG("bear.png")
+  my.symbols( 1.04, -0.36, ms.image, MoreArgs=list(img=img),
+             inches=0.2, symb.plots=TRUE, add=TRUE)
+}
 
-## dev.copy(pdf,'search5.pdf')
-## dev.off()
+dev.copy(pdf,'search5.pdf')
+dev.off()
 
 
 ## ## -----------------------------------

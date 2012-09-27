22e52bf67541c551e03cf054003bc4a1479cbc77
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-09-27 04:37:24 +0000

    feedPDE:

--- trunk/problem/SingleCellGrow/vwrelation.r
+++ trunk/problem/SingleCellGrow/vwrelation.r
@@ -2,9 +2,9 @@ dyn.load('simulator1.so')
 source('simulator1s.r')
 
 T <- 8
-r1 <- 1
+r1 <- 1.1
 r2 <- 0.6
-d1 <- 0.3
+d1 <- 0.4
 d2 <- 0.3
 v <- 0.5
 
@@ -15,10 +15,14 @@ for ( i in seq(N) ){
   x <- simulator1s(T, r1, r2, d1, d2, v, 1)
   ## cat(x,'\n')
   if ( sum(x) > 100 ){
-    n <- n+1    
+    n <- n+1
     w[n] <- x[1]/sum(x)
   }
 }
 
-den <- density(w[1:n])
-plot(den)
+#den <- density(w[1:n])
+#plot(den)
+
+Fe <- ecdf(w[1:n])
+plot(Fe)
+abline(h=c(0.975, 0.025))

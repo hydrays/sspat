5b6cee2b2f966f979744ba9bc882e5f991880250
hydrays <hydrays@gmail.com>
2012-10-05 18:05:24 +0000

    feedPDE: a succesful one?

--- problem/feedPDE/exact_1d/setting.f90
+++ problem/feedPDE/exact_1d/setting.f90
@@ -1,6 +1,6 @@
 module setting
   implicit none
-  integer, parameter :: nx = 256
+  integer, parameter :: nx = 128
   integer, parameter :: n = nx+2
   integer, parameter :: n1 = nx+3
   real x(0:n1)
@@ -126,9 +126,11 @@ contains
        do i = 2, n-1
           !press(i) = phi_TC_old(i) + phi_SC_old(i) + phi_MC_old(i)
           press(i) = phi_SC_old(i) + phi_MC_old(i)
-          if (press(i) > 0.65) then
-             press(1:i) = press(1:i) + 0.05
-          end if
+!!$          if (press(i) > 0.65) then
+!!$             press(1:i) = press(1:i) + 0.05
+!!$          end if
+          !press(i) = press(i) / (1.0 - 0.1*press(i))
+          press(i) = press(i)
        end do
 
 !!$       do i = 2, n-1
@@ -139,22 +141,33 @@ contains
 
        do i = 2, n-1
           p0(i) = 1.0 / (l_d + gain1*TGF(i))
-          q(i) = 1.0/(1.0 + 2.0*press(i))
+          if ( press(i) > 0.8) then
+             q(i) = 0.0
+          else
+             q(i) = 1.0
+          end if
+          !q(i) = 1.0/(1.0 + 2.0*press(i))
           d(i) = max(0.0, xi*(press(i)-0.5))
           v0(i) = 1 / (1.0/v0max + gain1*TGF(i)*(1.0/v0min - 1.0/v0max))
           C1(i) = q(i)*v0(i)*(2.0*p0(i)-1.0)*phi_SC_old(i) - d(i)*phi_SC_old(i)
           C2(i) = (2.0*(1-p0(i)))*phi_SC_old(i) - &
                v_tc*phi_TC_old(i)
-          if ( time > 15 ) then
-             if ( sum(phi_MC_old(2:n-1)) > 0.1 ) then
-                !print *, time, sum(phi_MC_old(2:n-1))
-                C2(2:10) = C2(2:10) - 10.0*phi_TC_old(2:10)
-             else
-                phi_MC_old(2:n-1) = 0.0
-             end if
-          end if
+!!$          if ( time > 15 ) then
+!!$             if ( sum(phi_MC_old(2:n-1)) > 0.01 ) then
+!!$                !print *, time, sum(phi_MC_old(2:n-1))
+!!$                C2(2:5) = C2(2:5) - 10.0*phi_TC_old(2:5)
+!!$             else
+!!$                phi_MC_old(2:n-1) = 0.0
+!!$             end if
+!!$          end if
           C3(i) = q(i)*v_m*(2.0*p_m-1.0)*phi_MC_old(i) - d(i)*phi_MC_old(i)
        enddo
+
+       do i = 2, n-1
+          if (phi_MC_old(i) > tol ) then
+             C2(i) = C2(i) - 1000.0*phi_MC_old(i)*phi_TC_old(i)
+          end if
+       end do
 
        do i = 3, n-2
           dTdx_west = (phi_SC_old(i) - phi_SC_old(i-1))/dx
@@ -290,7 +303,7 @@ contains
        !write(11,'(10(e16.4e3))') phi_SC(i), phi_TC(i), &
        !     phi_MC(i), p0(i), TGF(i), d(i), v0(i), press(i)
        write(11,'(10(e16.4e3))') phi_SC(i), phi_TC(i), &
-            phi_MC(i), d(i), q(i), &
+            phi_MC(i), TGF(i), q(i), &
             q(i)*v0(i)*(2.0*p0(i)-1.0)-d(i), &
             q(i)*v_m*(2.0*p_m-1.0)-d(i), press(i)
     end do

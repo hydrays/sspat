f52bf3b12db41c1c8f57d7b219cdafa9f159f515
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2012-10-27 02:19:22 +0000

    feedPDE/exact_1d: an almost working setting, so let's make a backup--- revert to this one

--- trunk/problem/feedPDE/exact_1d/out/myplot.r
+++ trunk/problem/feedPDE/exact_1d/out/myplot.r
@@ -25,33 +25,33 @@ p1 <- xyplot(c(0,max(z))~c(0, max(x)),
              type='n', xlab="", ylab="",
              scales=list(cex=2),
              key = list(x = 0.01, y=0.99,
-               border=TRUE,
-               lines=list(
-                 cex = 1,
-                 pch=c(1,1,1,NA,NA,NA,NA),
-                 col=c("red","green","black","red","blue","black","green"),
-                 type=c("b","b","b","l","l","l","l"),
-                 lty=c(1,1,1,2,2,2,2),
-                 lwd=c(1,1,1,2,2,2,2)),
-               text = list(lab = c("SC","TC","MC","p0","TGFb","d","v0")),
-               columns = 4,
-               title = NULL,
-               space = "top"
-               ),
+             border=TRUE,
+             lines=list(
+             cex = 1,
+             pch=c(1,1,1,NA,NA,NA,NA),
+             col=c("red","green","black","red","blue","black","green"),
+             type=c("b","b","b","l","l","l","l"),
+             lty=c(1,1,1,2,2,2,2),
+             lwd=c(1,1,1,2,2,2,2)),
+             text = list(lab = c("SC","TC","MC","p0","TGFb","d","v0")),
+             columns = 4,
+             title = NULL,
+             space = "top"
+             ),
              panel=function(...){
-               panel.lines(x, z[,1], type='b', col='red')
-               panel.lines(x, z[,2], type='b', col='green')
-               panel.lines(x, z[,3], type='b', col='black')
-               panel.lines(x, z[,4], type='l',
-                           lwd = 2, lty = 2, col='red')
-               panel.lines(x, z[,5], type='l',
-                           lwd = 2, lty=2, col='blue')
-               panel.lines(x, z[,6], type='l',
-                           lwd = 2, lty=2, col='black')
-               panel.lines(x, z[,7], type='l',
-                           lwd = 2, lty=2, col='green')
-               grid.text(my.label.time,
-                         y = unit(0.9, "npc"), gp=gpar(fontsize=30))
+                 panel.lines(x, z[,1], type='b', col='red')
+                 panel.lines(x, z[,2], type='b', col='green')
+                 panel.lines(x, z[,3], type='b', col='black')
+                 panel.lines(x, z[,4], type='l',
+                             lwd = 2, lty = 2, col='red')
+                 panel.lines(x, z[,5], type='l',
+                             lwd = 2, lty=2, col='blue')
+                 panel.lines(x, z[,6], type='l',
+                             lwd = 2, lty=2, col='black')
+                 panel.lines(x, z[,7], type='l',
+                             lwd = 2, lty=2, col='green')
+                  grid.text(my.label.time,
+                            y = unit(0.9, "npc"), gp=gpar(fontsize=30))
              }
              )
 
@@ -61,15 +61,15 @@ p2 <- xyplot(c(0,1)~c(0, 1),
              scales=list(alternating=0, tck=0, draw=FALSE),
              border=FALSE,
              panel=function(...){
-               panel.xyplot(...)
-               for ( j in seq(nrow(parainfo)) ){
-                 grid.text(paste("# ", parainfo[j,1],
-                                 " = ", parainfo[j,2]),
-                           just="left",
-                           x = unit(0.01 + 0.2*((j-1)%%1), "npc"),
-                           y = unit(0.9-0.04*as.integer((j-1)/1), "npc"),
-                           gp=gpar(fontsize=16) )
-               }
+                 panel.xyplot(...)
+                 for ( j in seq(nrow(parainfo)) ){
+                     grid.text(paste("# ", parainfo[j,1],
+                                     " = ", parainfo[j,2]),
+                               just="left",
+                               x = unit(0.01 + 0.2*((j-1)%%1), "npc"),
+                               y = unit(0.9-0.04*as.integer((j-1)/1), "npc"),
+                               gp=gpar(fontsize=16) )
+                 }
              }
              )
 
@@ -78,97 +78,59 @@ print(p2, position=c(0, 0.0, 0.2, 0.9))
 
 for (i in seq(N)) {
 
-  datafile <- sprintf("%s%05d%s", "m", i, ".dat")
-  outfile <- sprintf("%s%05d%s", "slice", i, ".png")
-  png(outfile, width=.pwidth, height=.pheight)
-  z <- matrix(scan(datafile, quiet=TRUE), ncol=NumCol, byrow=TRUE)
-  my.label.time <- sprintf("%s%d%s", "t = ", as.integer(i*.tpinc), " (day)")
+    datafile <- sprintf("%s%05d%s", "m", i, ".dat")
+    outfile <- sprintf("%s%05d%s", "slice", i, ".png")
+    png(outfile, width=.pwidth, height=.pheight)
+z <- matrix(scan(datafile, quiet=TRUE), ncol=NumCol, byrow=TRUE)
+my.label.time <- sprintf("%s%d%s", "t = ", as.integer(i*.tpinc), " (day)")
 
-  p1 <- xyplot(c(0,2)~c(0, max(x)),
-               type='n', xlab="", ylab="",
-               scales=list(cex=2),
-               key = list(x = 0.8, y=0.9,
-               border=FALSE,
-               lines=list(
-               pch=c(1,1,1,NA,NA,NA,NA,NA),
-               col=c("red","green","black","red","black",
-               "grey","green","red","black"),
-               type=c("b","b","b","l","l","l","l","l","l"),
-               lty=c(1,1,1,2,2,3,3,3,3),
-               lwd=c(1,1,1,2,0,0,0,0,0)),
-               cex = 2,
-               text = list(lab = c("SC","TC","MC","TGFb",
-                           "","","","","")),
-               columns = 1,
-               #space = ""
-               title = NULL
-               ),
-               panel=function(...){
-                   panel.lines(x, z[,1], type='b', col='red')
-                   panel.lines(x, z[,2], type='b', col='green')
-                   panel.lines(x, z[,3], type='b', col='black')
-                   panel.lines(x, z[,4], type='l',
-                               lwd = 4, lty = 2, col='pink')
-                   #panel.lines(x, z[,5], type='l',
-                   #            lwd = 1, lty=1, col='blue')
-                   panel.lines(x, z[,6], type='l',
-                               lwd = 2, lty=2, col='red')
-                   panel.lines(x, z[,7], type='l',
-                               lwd = 2, lty=2, col='black')
-                   panel.lines(x, z[,8], type="l",
-                               lwd = 6, lty="dotted", col='grey')
-                   grid.text(my.label.time,
-                             y = unit(0.9, "npc"), gp=gpar(fontsize=30))
-               }
-               )
+p1 <- xyplot(c(-1,3)~c(0, max(x)),
+             type='n', xlab="", ylab="",
+             scales=list(cex=2),
+             key = list(x = 0.4, y=0.8,
+             border=FALSE,
+             lines=list(
+             pch=c(1,1,1,NA,NA,NA,NA,NA),
+             col=c("red","green","black","red","black",
+             "grey","green","red","black"),
+             type=c("b","b","b","l","l","l","l","l","l"),
+             lty=c(1,1,1,2,2,3,3,3,3),
+             lwd=c(1,1,1,2,2,6,0,0,0)),
+             cex = 1,
+             text = list(lab = c("SC","TC","MC","net growth rate of SC",
+                         "net growth rate of MC","pressure","","","")),
+             columns = 4,
+             #space = ""
+             title = NULL
+             ),
+             panel=function(...){
+                 panel.lines(x, z[,1], type='b', col='red')
+                 panel.lines(x, z[,2], type='b', col='green')
+                 panel.lines(x, z[,3], type='b', col='black')
+                 panel.lines(x, z[,4], type='l',
+                             lwd = 4, lty = 2, col='pink')
+                 #panel.lines(x, z[,5], type='l',
+                 #            lwd = 1, lty=1, col='blue')
+                 panel.lines(x, z[,6], type='l',
+                             lwd = 2, lty=2, col='red')
+                 panel.lines(x, z[,7], type='l',
+                             lwd = 2, lty=2, col='black')
+                 #panel.lines(x, z[,7]*(2*z[,4]-1), type="l",
+                 #            lwd = 4, lty="dotted", col='red')
+                 #panel.lines(x, .vm*(2*.pm-1), type="l",
+                 #            lwd = 4, lty="dotted", col='black')
+                 panel.lines(x, z[,8], type="l",
+                             lwd = 6, lty="dotted", col='grey')
+                  grid.text(my.label.time,
+                            y = unit(0.9, "npc"), gp=gpar(fontsize=30))
+             }
+             )
 
+print(p1, position=c(0.2, 0, 1, 1), more=TRUE)
+print(p2, position=c(0, 0, 0.2, 0.9))
 
-  ## p1 <- xyplot(c(0,2)~c(0, max(x)),
-  ##              type='n', xlab="", ylab="",
-  ##              scales=list(cex=2),
-  ##              key = list(x = 0.8, y=0.9,
-  ##                border=FALSE,
-  ##                lines=list(
-  ##                  pch=c(NA,NA,NA,NA,NA,NA,NA,NA),
-  ##                  col=c("red","black","blue","grey",
-  ##                    "grey","green","red","black"),
-  ##                  type=c("l","l","l","l","l","l","l","l","l"),
-  ##                  lty=c(1,1,1,2,2,3,3,3,3),
-  ##                  lwd=c(1,1,1,6,0,0,0,0,0)),
-  ##                cex = 2,
-  ##                text = list(lab = c("v0","vm","p0",
-  ##                              "pressure","", "", "")),
-  ##                columns = 1,
-  ##                                       #space = ""
-  ##                title = NULL
-  ##                ),
-  ##              panel=function(...){
-  ##                #panel.lines(x, z[,1], type='b', col='red')
-  ##                #panel.lines(x, z[,2], type='b', col='green')
-  ##                #panel.lines(x, z[,3], type='b', col='black')
-  ##                #panel.lines(x, z[,4], type='l',
-  ##                #            lwd = 4, lty = 2, col='pink')
-  ##                panel.lines(x, z[,4], type='l',
-  ##                            lwd = 2, lty=1, col='red')
-  ##                panel.lines(x, z[,5], type='l',
-  ##                            lwd = 2, lty=1, col='black')
-  ##                panel.lines(x, z[,6], type='l',
-  ##                            lwd = 2, lty=1, col='blue')
-  ##                #panel.lines(x, z[,7], type='l',
-  ##                #            lwd = 2, lty=2, col='blue')
-  ##                panel.lines(x, z[,8], type="l",
-  ##                            lwd = 6, lty="dotted", col='grey')
-  ##                                       #grid.text(my.label.time,
-  ##                                       #          y = unit(0.9, "npc"), gp=gpar(fontsize=30))
-  ##              }
-  ##              )
-
-  print(p1, position=c(0.2, 0, 1, 1), more=TRUE)
-  print(p2, position=c(0, 0, 0.2, 0.9))
-  #print(p1)
-  
-  output.str1 <- sprintf("%5d", i)
-  if (i > 1) cat("\b\b\b\b\b")
-  cat(output.str1)
-  dev.off()
+    output.str1 <- sprintf("%5d", i)
+    if (i > 1) cat("\b\b\b\b\b")
+    cat(output.str1)
+    dev.off()
 }
--- trunk/problem/feedPDE/exact_1d/setting.f90
+++ trunk/problem/feedPDE/exact_1d/setting.f90
@@ -1,6 +1,6 @@
 module setting
   implicit none
-  integer, parameter :: nx = 256
+  integer, parameter :: nx = 128
   integer, parameter :: n = nx+2
   integer, parameter :: n1 = nx+3
   real x(0:n1)
@@ -58,9 +58,8 @@ contains
           TGF(i)   = TGF_old(i) + dTdt*dt_TGF
        enddo
        ! boundary conditions
-       ! west Direchlet
+       ! west Neumann
        i = 2
-       !dTdx_w = (-TGF_old(i+1) + 9*TGF_old(i) - 8.0*TGF_old(i-1))/(3*dx)
        dTdx_w = 0.0
        dTdx_e = (TGF_old(i+1) - TGF_old(i))/dx
        dTdt   = alpha_TGF*(dTdx_e - dTdx_w)/dx + C_TGF(i)
@@ -68,8 +67,7 @@ contains
        ! east Direchlet
        i = n-1
        dTdx_w = (TGF_old(i) - TGF(i-1))/dx
-       !dTdx_e = (8*TGF_old(i+1) - 9*TGF_old(i) + TGF_old(i-1))/(3*dx)
-       dTdx_e = 0.0
+       dTdx_e = (8*TGF_old(i+1) - 9*TGF_old(i) + TGF_old(i-1))/(3*dx)
        dTdt   = alpha_TGF*(dTdx_e - dTdx_w)/dx + C_TGF(i)
        TGF(i) = TGF_old(i) + dTdt*dt_TGF
 
@@ -115,7 +113,7 @@ contains
        endif
 
        if (time .ge. tm) then
-          phi_MC_old(nx/2) = 0.01
+          phi_MC_old(2) = 0.01
           tm = tm + tfinal
           !write(*,*), 'mutation time'
           !read(*,*)
@@ -147,9 +145,9 @@ contains
 !!$          else
 !!$             q(i) = 1.0
 !!$          end if
-          q(i) = 1.0 / (1.0 + exp(1000.0*(press(i)-0.3)))
+          q(i) = 1.0 / (1.0 + exp(100.0*(press(i)-0.8)))
           !q(i) = 1.0/(1.0 + 2.0*press(i))
-          d(i) = max(0.0, xi*(press(i)-0.2))
+          d(i) = max(0.0, xi*(press(i)-0.8))
           v0(i) = 1 / (1.0/v0max + gain1*TGF(i)*(1.0/v0min - 1.0/v0max))
           C1(i) = q(i)*v0(i)*(2.0*p0(i)-1.0)*phi_SC_old(i) - d(i)*phi_SC_old(i)
           C2(i) = (2.0*(1-p0(i)))*phi_SC_old(i) - &
@@ -165,11 +163,11 @@ contains
           C3(i) = q(i)*v_m*(2.0*p_m-1.0)*phi_MC_old(i) - d(i)*phi_MC_old(i)
        enddo
 
-!!$       do i = 2, n-1
-!!$          if (time > 30.0) then
-!!$             C2(i) = C2(i) - 10.0*phi_MC_old(i)*phi_TC_old(i)
-!!$          end if
-!!$       end do
+       do i = 2, n-1
+          if (time > 120.0) then
+             C2(i) = C2(i) - 1000.0*phi_MC_old(i)*phi_TC_old(i)
+          end if
+       end do
 
        do i = 3, n-2
           dTdx_west = (phi_SC_old(i) - phi_SC_old(i-1))/dx
@@ -306,8 +304,8 @@ contains
        !     phi_MC(i), p0(i), TGF(i), d(i), v0(i), press(i)
        write(11,'(10(e16.4e3))') phi_SC(i), phi_TC(i), &
             phi_MC(i), TGF(i), d(i), &
-            q(i)*v0(i)*(2.0*p0(i)-1.0), &
-            q(i)*v_m*(2.0*p_m-1.0), press(i)
+            q(i)*v0(i)*(2.0*p0(i)-1.0) - d(i), &
+            q(i)*v_m*(2.0*p_m-1.0) - d(i), press(i)
     end do
 
     close(11)
@@ -369,8 +367,7 @@ contains
        ! do nothing
     endif
 
-    TGF(n) = 0.0
-    TGF(1) = 0.0
+    TGF(n) = 0.2
   end SUBROUTINE BOUNDARY_COND
 
   SUBROUTINE INITIAL_COND
@@ -379,10 +376,9 @@ contains
     ! defining the initial conditions
     ! dsin(pi*x/xlen)
     do i = 1, n
-       phi_SC(i) = 0.264
-       phi_TC(i) = 1.32
+       phi_SC(i) = 0.1 + 0.05*sin(x(i))
+       phi_TC(i) = 1.0
        phi_MC(i) = 0.0
-       TGF(i) = 0.66
     enddo
   end SUBROUTINE INITIAL_COND
 

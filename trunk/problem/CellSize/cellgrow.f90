26b2238ea962200dfc91b5fa077d1df2af135e60
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2013-05-10 05:33:37 +0000

    CellSize: first version but wrong version! This version include nActiveRibsome as an independent variable. Now I just make it to be min(mRNA, Ribsome).

--- trunk/problem/CellSize/cellgrow.f90
+++ trunk/problem/CellSize/cellgrow.f90
@@ -8,9 +8,12 @@ contains
     use setting
     implicit none
 
-    real t, tau, tp, u
-    integer output_index, i, j, active_index
-    integer k, shift_i
+    real t, tp, u
+    real lsize, lage
+    integer output_index, i, j, r(lReac)
+    integer is_nag, m_flag
+    real a(lReac), lx(lSpec)
+
     open (unit = 100, file='./out/logfile', action="write")
     call ran_seed(sequence=iseed)
     call init_cell_pool()
@@ -20,38 +23,68 @@ contains
     output_index = 0
 
     do while (t < tend)
+       
+       !print *, t
+       
        if (t .ge. tp) then
           call output_to_file(output_index)
-          call cell_stat(t)
           output_index = output_index + 1
           tp = tp + tpinc
+          print *, t
        end if
 
-       call Next_Reaction(k, tau)
+       do i = 1, NPool
+          lsize = CellPool(i)%Csize
+          lage = CellPool(i)%Cage
+          lx(1) = CellPool(i)%mRNA
+          lx(3) = CellPool(i)%nRibsome
+          lx(2) = min(lx(1), lx(3))
 
-       call cell_event(k)
+          call getrate(lx, lage, a)
+          do j = 1, lReac
+             r(j) = poidev(a(j)*timestep)
+             lx = lx + nu(:, j)*r(j)
 
-       do i = 1, L
-          NT(i) = NT(i) + a(i)*tau
-       end do
 
-       do i = -2, 2
-          shift_i = k + i
-          if ( shift_i .le. 0 ) then
-             shift_i = shift_i + L
-          else if ( shift_i > L ) then
-             shift_i = shift_i - L
+             ! if ( j .eq. 3 ) then
+             !    if ( r(j) > lx(1) ) then
+             !       r(j) = lx(1)
+             !    end if
+             !    if ( r(j) + lx(2) > lx(3) ) then
+             !       r(j) = lx(3) - lx(2)
+             !    end if
+             ! end if
+             ! if ( j .eq. 4 .and. r(j) > lx(2) ) then
+             !    r(j) = lx(2)
+             ! end if
+
+             !print *, j, r(j), a(j)
+          end do
+          call checkx(lx, is_nag)          
+          if ( lsize < lx(3) ) then
+             CellPool(i)%Crate = 0.0
+          else
+             CellPool(i)%Crate = (a(5) - a(6))
           end if
-          call Update_Rate(shift_i)
+          lsize = max(lsize, lx(3))
+          lage = lage + timestep
+          CellPool(i)%Csize = lsize
+          CellPool(i)%Cage = lage
+          CellPool(i)%mRNA = lx(1)
+          CellPool(i)%nActiveRibsome = lx(2)
+          CellPool(i)%nRibsome = lx(3)
+          
+          call check_mitosis(lsize, lage, m_flag)
+          if ( m_flag .eq. 1 ) then
+             call cell_division(i)
+          end if
        end do
+       t = t + timestep
 
-       call expdev(u)
-       if ( u.eq.0 ) then
-          print *, 'exp value = 0'
-          read(*,*)
-       end if
-       NP(k) = NP(k) + u
-       t = t + tau
+       print *, t, CellPool(1)%Csize, CellPool(1)%Cage, & 
+            CellPool(1)%Crate, CellPool(1)%mRNA, &
+            CellPool(1)%nActiveRibsome, CellPool(1)%nRibsome 
+       if (CellPool(1)%Cage.eq.0.0) read(*,*)
 
     end do
     close(unit=100)

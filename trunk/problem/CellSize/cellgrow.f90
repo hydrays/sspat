42671e8eb47c10f49fb6b4f44da8bbe53407f7a7
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2013-05-11 18:07:18 +0000

    CellSize: prepare to get result.

--- trunk/problem/CellSize/cellgrow.f90
+++ trunk/problem/CellSize/cellgrow.f90
@@ -13,6 +13,13 @@ contains
     integer output_index, i, j, r(lReac)
     integer is_nag, m_flag
     real a(lReac), lx(lSpec)
+    integer n_mitosis, n_newborn, n_newborn2
+    integer num_livecell, first_record
+    real prodiv(NSize)
+    real basediv1(NSize), basediv2(NSize)
+    real ls, us, ds
+    integer Cindex
+    character(30) filename
 
     open (unit = 100, file='./out/logfile', action="write")
     call ran_seed(sequence=iseed)
@@ -22,6 +29,9 @@ contains
     tp = 0.0
     tm = 0.0
     output_index = 0
+    n_mitosis = 0
+    n_newborn = 0
+    n_newborn2 = 0
 
     do while (t < tend)
        
@@ -68,7 +78,23 @@ contains
           
           call check_mitosis(lsize, lage, m_flag)
           if ( m_flag .eq. 1 ) then
+             ! Collect the size of mitosis cell
+             if ( t > tc .and. n_mitosis < NCollect) then
+                n_mitosis = n_mitosis + 1
+                MitosisPool(n_mitosis) = CellPool(i)
+             end if
              call cell_division(i)
+             ! Collect the newborn cells
+             if ( t > tc .and. n_newborn < NCollect) then
+                n_newborn = n_newborn + 1
+                NewbornPool(n_newborn) = CellPool(i)
+             end if
+
+             if ( t > tc .and. n_newborn2 < NCollect2) then
+                n_newborn2 = n_newborn2 + 1
+                NewbornPool2(n_newborn2) = CellPool(i)
+             end if
+
           end if
        end do
        t = t + timestep
@@ -77,8 +103,143 @@ contains
        !     CellPool(1)%Crate, CellPool(1)%mRNA, CellPool(1)%nRibsome 
        !if (CellPool(1)%Cage.eq.0.0) read(*,*)
 
+       ! Collect new born cell
+
+          
     end do
     close(unit=100)
+
+    ! Output mitosis cell to file
+    if ( n_mitosis < NCollect ) then
+       print *, "Not enough mitosis cells are collected..."
+       read(*,*)
+    else
+       call output_to_file(-1)
+    end if
+
+    ! Output newborn cell to file
+    if ( n_newborn < NCollect ) then
+       print *, "Not enough newborn cells are collected..."
+       read(*,*)
+    else
+       call output_to_file(-2)
+    end if
+
+    ! Check if NewbornPool2 is filled
+    if ( n_newborn2 < NCollect2 ) then
+       print *, "Not enough newborn cells are collected..."
+       read(*,*)
+    end if
+
+    ! Continue growing the newborn cells syncrolized
+    t = 0.0
+    tp = 0.0
+    output_index = 0
+    do while (t < tend)
+       if (t .ge. tp) then
+          call output_to_file_syn(output_index)
+          output_index = output_index + 1
+          tp = tp + tpinc
+          print *, t
+       end if
+
+       !print *, "1"
+       do i = 1, NCollect
+          lsize = NewbornPool(i)%Csize
+          lage = NewbornPool(i)%Cage
+          lx(1) = NewbornPool(i)%mRNA
+          lx(2) = NewbornPool(i)%nRibsome
+
+          call getrate(lx, lage, a)
+          do j = 1, lReac
+             r(j) = poidev(a(j)*timestep)
+             if (r(2) > lx(1) ) then
+                r(2) = lx(1)
+             end if
+             lx = lx + nu(:, j)*r(j)
+             !print *, j, r(j), a(j)
+          end do
+          call checkx(lx, a, r, is_nag)          
+          lsize = max(lsize, lx(2))
+          lage = lage + timestep
+          NewbornPool(i)%Csize = lsize
+          NewbornPool(i)%Cage = lage
+          NewbornPool(i)%mRNA = lx(1)
+          NewbornPool(i)%nRibsome = lx(2)
+          
+          call check_mitosis(lsize, lage, m_flag)
+          if ( m_flag .eq. 1 ) then
+             !print *, "2"
+             call cell_division_syn(i)
+          end if
+       end do
+       !print *, "3"
+       t = t + timestep
+    end do
+
+    ! Make data for Figure 4.
+    t = 0.0
+    tp = 0.0
+    tm = 0.0
+    open (unit = 12, file='./out/trace.dat', action="write")
+    do while (t < 24)
+       do i = 1, NCollect2
+          if ( NewbornPool2(i)%Csize > 0.0 ) then
+
+             lsize = NewbornPool2(i)%Csize
+             lage = NewbornPool2(i)%Cage
+             lx(1) = NewbornPool2(i)%mRNA
+             lx(2) = NewbornPool2(i)%nRibsome
+
+             call getrate(lx, lage, a)
+             do j = 1, lReac
+                r(j) = poidev(a(j)*timestep)
+                if (r(2) > lx(1) ) then
+                   r(2) = lx(1)
+                end if
+                lx = lx + nu(:, j)*r(j)
+                !print *, j, r(j), a(j)
+             end do
+             call checkx(lx, a, r, is_nag)          
+             lsize = max(lsize, lx(2))
+             lage = lage + timestep
+             NewbornPool2(i)%Csize = lsize
+             NewbornPool2(i)%Cage = lage
+             NewbornPool2(i)%mRNA = lx(1)
+             NewbornPool2(i)%nRibsome = lx(2)
+             
+             call check_mitosis(lsize, lage, m_flag)
+             if ( m_flag .eq. 1 ) then
+                NewbornPool2(i)%Csize = -1.0
+                print *, i, ' cell die ...'
+             end if
+             
+          end if
+       end do
+       t = t + timestep
+
+       if ( t > tp ) then
+          WRITE(filename,'(A12,I2.2,A4)') './out/prodiv', floor(tm), '.dat'
+          open (unit = 11, file=filename, action="write")
+          do i = 1, NCollect2
+             write(11, '(6(F16.2))'), NewbornPool2(i)%Csize
+             write(*, '(6(F16.2))'), prodiv(i)
+          end do
+          close(11)
+          tp = tp + 1.0
+       end if
+
+       ! Plot trace
+       if ( t > tm ) then
+          do i = 1, 1000
+             write(12, '((F16.2))', advance='no'), NewbornPool2(i)%Csize
+             !write(*, '(6(F16.2))'), prodiv(i)
+          end do
+          write (12, *)
+          tm = tm + tminc
+       end if
+    end do
+    close(12)
 
   end subroutine cell_grow
 
--- trunk/problem/CellSize/out/myplot.r
+++ trunk/problem/CellSize/out/myplot.r
@@ -1,23 +1,27 @@
 library("lattice")
 library("grid")
+jet.colors <- colorRampPalette(c("white", "red", "blue", "green"))
 
 parainfo <- read.csv("control.csv", strip.white=TRUE)
 NPool <- parainfo$VALUE[parainfo$PARAMETER=='NPool']
+NCollect <- parainfo$VALUE[parainfo$PARAMETER=='NCollect']
+NCollect2 <- parainfo$VALUE[parainfo$PARAMETER=='NCollect2']
 .tend <- parainfo$VALUE[parainfo$PARAMETER=='tend']
 .tpinc <- parainfo$VALUE[parainfo$PARAMETER=='tpinc']
 
 N = 200
 pL = 400
 pH = 60
-.pwidth = 1064
-.pheight = 600
+.pwidth = 800
+.pheight = 640
 
 cat("processing file ...[",N,"]\n")
 i <- 190
 datafile <- sprintf("%s%05d%s", "m", i, ".dat")
 
 outfile <- sprintf("%s%05d%s", "slice", i, ".png")
-png(outfile, width=340, height=300)
+png(outfile, width=.pwidth, height=.pheight)
+#png(outfile, width=340, height=300)
 z <- matrix(scan(datafile, n=NPool*5, quiet=TRUE),
             NPool, 5, byrow=TRUE)
 my.label.time <- sprintf("%s%d%s", "t = ", i, " (day)")
@@ -26,7 +30,8 @@ print(p1)
 dev.off()
 
 outfile <- sprintf("%s%05d%s", "fa", i, ".png")
-png(outfile, width=1080, height=640)
+png(outfile, width=.pwidth, height=.pheight)
+#png(outfile, width=1080, height=640)
 my.label.time <- sprintf("%s%d%s", "t = ", i, " (day)")
 p1 <- histogram(z[,1], nint=30)
 print(p1)
@@ -34,7 +39,8 @@ dev.off()
 
 
 outfile <- sprintf("%s%05d%s", "rate", i, ".png")
-png(outfile, width=340, height=300)
+png(outfile, width=.pwidth, height=.pheight)
+#png(outfile, width=340, height=300)
 g <- 200*(z[,1]-z[,2])/max(z[,1]-z[,2])
 index <- 0
 Nbin <- 100
@@ -54,7 +60,7 @@ for (i in seq(Nbin)){
 for (i in seq(NPool)){
   z[i,1] = min(ux, z[i,1])
   z[i,1] = max(lx, z[i,1])     
-  index <- floor((z[i,1]-lx)/dx)
+  index <- floor((z[i,1]-lx)/dx)+1
   nn[index] <- nn[index] + 1
   v[index] <- v[index] + g[i]*g[i]
   m[index] <- m[index] + g[i]
@@ -67,6 +73,151 @@ p1 <- xyplot(m~x, xlim=c(0, ux), grid=TRUE)
 print(p1)
 dev.off()
 
+## Plot histogram for mitosis cells.
+datafile <- sprintf("CellMitosis.dat")
+outfile <- sprintf("mitosis.png")
+png(outfile, width=.pwidth, height=.pheight)
+z <- matrix(scan(datafile, n=NPool*5, quiet=TRUE),
+            NPool, 5, byrow=TRUE)
+p1 <- histogram(z[,1], nint=30)
+print(p1)
+dev.off()
+
+## Plot histogram for newborn cells.
+datafile <- sprintf("CellNewborn.dat")
+outfile <- sprintf("Newborn.png")
+png(outfile, width=.pwidth, height=.pheight)
+z <- matrix(scan(datafile, n=NPool*5, quiet=TRUE),
+            NPool, 5, byrow=TRUE)
+p1 <- histogram(z[,1], nint=30)
+print(p1)
+dev.off()
+
+## Plot trajectory of the newborn cells syncronized.
+outfile <- sprintf("syn.png")
+png(outfile, width=.pwidth, height=.pheight)
+ls <- 0
+us <- 560000
+T <- 24
+M <- 80
+ds <- (us-ls)/M
+data = matrix(0, T, M)
+
+for (i in seq(T)) {
+  datafile <- sprintf("%s%05d%s", "s", i, ".dat")
+  z <- matrix(scan(datafile, n=NCollect*5, quiet=TRUE),
+              NCollect, 5, byrow=TRUE)
+  for (j in seq(NCollect)){
+    z[j,1] = min(us, z[j,1])
+    z[j,1] = max(ls, z[j,1])         
+    Mindex <- floor( (z[j,1]-ls)/ds ) + 1
+    #print(Mindex)
+    data[i, Mindex] <- data[i, Mindex] + 1
+  }
+}
+
+M <- floor((280000 - ls)/ds + 1)
+p1 <- levelplot(data[1:T, 1:M], col.regions=jet.colors,
+                colorkey=TRUE, xlab="",
+                aspect="fill",
+                ylab="",
+                scales=list(cex=2))
+print(p1)
+dev.off()
+
+## Make figure 4
+outfile <- sprintf("figure4.png")
+png(outfile, width=.pwidth, height=.pheight)
+ls <- 140000
+us <- 260000
+M <- 20
+ds <- (us-ls)/M
+data1 <- seq(M)
+data2 <- seq(M)
+x <- seq(M)
+num1 <- seq(M)
+num2 <- seq(M)
+
+datafile1 <- sprintf("%s%02d%s", "prodiv", 6, ".dat")
+datafile2 <- sprintf("%s%02d%s", "prodiv", 7, ".dat")
+z1 <- matrix(scan(datafile1, n=NCollect2*1, quiet=TRUE),
+            NCollect2, 1, byrow=TRUE)
+z2 <- matrix(scan(datafile2, n=NCollect2*1, quiet=TRUE),
+            NCollect2, 1, byrow=TRUE)
+for (i in seq(M)){
+  num1[i] <- 0
+  num2[i] <- 0
+}
+
+for (j in seq(NCollect2)){
+    if ( z1[j,1] > 0 ) {
+      z1[j,1]  = min(us, z1[j,1])
+      z1[j,1] = max(ls, z1[j,1])         
+      Mindex <- floor( (z1[j,1]-ls)/ds ) + 1
+      num1[Mindex] = num1[Mindex] + 1
+      if (z2[j,1] > 0 ) {
+        num2[Mindex] = num2[Mindex] + 1
+      }
+    }
+  }
+for ( i in seq(M) ) {
+  x[i] = ls + i*ds - 0.5*ds
+  data1[i] <- 1 - num2[i]/num1[i]
+}
+
+datafile1 <- sprintf("%s%02d%s", "prodiv", 9, ".dat")
+datafile2 <- sprintf("%s%02d%s", "prodiv", 10, ".dat")
+z1 <- matrix(scan(datafile1, n=NCollect2*1, quiet=TRUE),
+            NCollect2, 1, byrow=TRUE)
+z2 <- matrix(scan(datafile2, n=NCollect2*1, quiet=TRUE),
+            NCollect2, 1, byrow=TRUE)
+for (i in seq(M)){
+  num1[i] <- 0
+  num2[i] <- 0
+}
+for (j in seq(NCollect2)){
+    if ( z1[j,1] > 0 ) {
+      z1[j,1]  = min(us, z1[j,1])
+      z1[j,1] = max(ls, z1[j,1])         
+      Mindex <- floor( (z1[j,1]-ls)/ds ) + 1
+      num1[Mindex] = num1[Mindex] + 1
+      if (z2[j,1] > 0 ) {
+        num2[Mindex] = num2[Mindex] + 1
+      }
+    }
+  }
+for ( i in seq(M) ) {
+  data2[i] <- 1 - num2[i]/num1[i]
+}
+plot(x, data1, type = 'b', ylim = c(0, 1))
+lines(x, data2, type = 'b', col='red', ylim=c(0,1))
+dev.off()
+
+
+## Trajecotries in the second paper
+outfile <- sprintf("trace.png")
+png(outfile, width=.pwidth, height=.pheight)
+z <- matrix(scan("trace.dat", n=25*1000, quiet=TRUE),
+            25, 1000, byrow=TRUE)
+x <- matrix(0, 25, 1)
+y <- matrix(0, 25, 1)
+plot(c(0, 25000), c(0, 4000))
+for ( j in seq(1000) ) {
+  if ( (z[1, j] - 10000)^2 < 400){
+  for (i in seq(25)) {
+    x[i] <- z[i, j]
+    if ( i < 2 ) {
+      y[i] <- 0
+    }
+    else {
+      y[i] <- z[i, j] - z[i-1, j]
+    }
+  }
+  lines(x, y, type="b", col=j)
+}
+}
+
+  
 ## for (i in seq(N)) {
 
 ##   datafile <- sprintf("%s%05d%s", "m", i, ".dat")

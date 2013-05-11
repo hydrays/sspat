c3a9109d643d3016f0b1883f2932ae8a342dac95
hydrays <hydrays@gmail.com>
2013-05-11 02:05:58 +0000

    CellSize: begin to collect data.

--- problem/CellSize/out/myplot.r
+++ problem/CellSize/out/myplot.r
@@ -32,6 +32,41 @@ p1 <- histogram(z[,1], nint=30)
 print(p1)
 dev.off()
 
+
+outfile <- sprintf("%s%05d%s", "rate", i, ".png")
+png(outfile, width=340, height=300)
+g <- 200*(z[,1]-z[,2])/max(z[,1]-z[,2])
+index <- 0
+Nbin <- 100
+lx <- 50000
+ux <- 250000
+dx <- (ux-lx)/Nbin
+m <- seq(Nbin)
+nn <- seq(Nbin)
+v <- seq(Nbin)
+x <- seq(Nbin)
+for (i in seq(Nbin)){
+  x[i] <- lx + dx*(i - 0.5)
+  m[i] <- 0
+  v[i] <- 0
+  nn[i] <- 0
+}
+for (i in seq(NPool)){
+  z[i,1] = min(ux, z[i,1])
+  z[i,1] = max(lx, z[i,1])     
+  index <- floor((z[i,1]-lx)/dx)
+  nn[index] <- nn[index] + 1
+  v[index] <- v[index] + g[i]*g[i]
+  m[index] <- m[index] + g[i]
+}
+for (i in seq(Nbin)){
+  m[i] <- m[i] / nn[i]
+  v[i] <- v[i] / nn[i] - m[i]*m[i]
+}
+p1 <- xyplot(m~x, xlim=c(0, ux), grid=TRUE)
+print(p1)
+dev.off()
+
 ## for (i in seq(N)) {
 
 ##   datafile <- sprintf("%s%05d%s", "m", i, ".dat")

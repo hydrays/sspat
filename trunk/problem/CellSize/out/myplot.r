91da2363aa402c1ce1e5bb456dc4574420fd1499
hydrays@gmail.com <hydrays@gmail.com@25bf473a-f1a1-c788-3f5b-03dbcf43837b>
2013-05-19 05:17:35 +0000

    CellSize: Now this is a working parameters.

--- trunk/problem/CellSize/out/myplot.r
+++ trunk/problem/CellSize/out/myplot.r
@@ -157,7 +157,7 @@ s.save <- s
 #outfile <- sprintf("%s%05d%s", "fa", i, ".png")
 #png(outfile, width=.pwidth, height=.pheight)
 #pdf(outfile, width=.pwidth, height=.pheight)
-pdf("fasyn.pdf", width=7, height=5)
+pdf("fasyn.pdf", width=7, height=7)
 datafile <- sprintf("%s%05d%s", "m", i, ".dat")
 z <- matrix(scan(datafile, n=NPool*L, quiet=TRUE),
             NPool, L, byrow=TRUE)
@@ -346,8 +346,8 @@ p1 <- levelplot(data[1:T, lM:M]/NCollect,
                 useRaster=TRUE,
                 region=FALSE,
                 colorkey=FALSE,
-                xlab=list("time (hours)", cex=2),
-                ylab=list("s (cell size, fl)", cex=2),
+                xlab=list("time (hours)", cex=1.5),
+                ylab=list("s (cell size, fl)", cex=1.5),
                 aspect="fill",
                 scales=list(cex=1.5,
                   y = list(relation="sliced",
@@ -359,89 +359,89 @@ p1 <- levelplot(data[1:T, lM:M]/NCollect,
 print(p1)
 dev.off()
 
-## Make figure 4
-#png("fig4.png", width=.pwidth, height=.pheight)
-pdf("fig4.pdf", width=7, height=7)
-ls <- 1400
-us <- 2600
-M <- 20
-ds <- (us-ls)/M
-data1 <- seq(M)
-data2 <- seq(M)
-x <- seq(M)
-num1 <- seq(M)
-num2 <- seq(M)
+## ## Make figure 4
+## #png("fig4.png", width=.pwidth, height=.pheight)
+## pdf("fig4.pdf", width=7, height=7)
+## ls <- 1400
+## us <- 2600
+## M <- 20
+## ds <- (us-ls)/M
+## data1 <- seq(M)
+## data2 <- seq(M)
+## x <- seq(M)
+## num1 <- seq(M)
+## num2 <- seq(M)
 
-datafile1 <- sprintf("%s%02d%s", "prodiv", 7, ".dat")
-datafile2 <- sprintf("%s%02d%s", "prodiv", 8, ".dat")
-z1 <- matrix(scan(datafile1, n=NCollect2*1, quiet=TRUE),
-            NCollect2, 1, byrow=TRUE)
-z2 <- matrix(scan(datafile2, n=NCollect2*1, quiet=TRUE),
-            NCollect2, 1, byrow=TRUE)
-for (i in seq(M)){
-  num1[i] <- 0
-  num2[i] <- 0
-}
+## datafile1 <- sprintf("%s%02d%s", "prodiv", 7, ".dat")
+## datafile2 <- sprintf("%s%02d%s", "prodiv", 8, ".dat")
+## z1 <- matrix(scan(datafile1, n=NCollect2*1, quiet=TRUE),
+##             NCollect2, 1, byrow=TRUE)
+## z2 <- matrix(scan(datafile2, n=NCollect2*1, quiet=TRUE),
+##             NCollect2, 1, byrow=TRUE)
+## for (i in seq(M)){
+##   num1[i] <- 0
+##   num2[i] <- 0
+## }
 
-for (j in seq(NCollect2)){
-    if ( z1[j,1] > 0 ) {
-      z1[j,1]  = min(us, z1[j,1])
-      z1[j,1] = max(ls, z1[j,1])         
-      Mindex <- floor( (z1[j,1]-ls)/ds ) + 1
-      num1[Mindex] = num1[Mindex] + 1
-      if (z2[j,1] > 0 ) {
-        num2[Mindex] = num2[Mindex] + 1
-      }
-    }
-  }
-for ( i in seq(M) ) {
-  x[i] = ls + i*ds - 0.5*ds
-  data1[i] <- 1 - num2[i]/num1[i]
-}
+## for (j in seq(NCollect2)){
+##     if ( z1[j,1] > 0 ) {
+##       z1[j,1]  = min(us, z1[j,1])
+##       z1[j,1] = max(ls, z1[j,1])         
+##       Mindex <- floor( (z1[j,1]-ls)/ds ) + 1
+##       num1[Mindex] = num1[Mindex] + 1
+##       if (z2[j,1] > 0 ) {
+##         num2[Mindex] = num2[Mindex] + 1
+##       }
+##     }
+##   }
+## for ( i in seq(M) ) {
+##   x[i] = ls + i*ds - 0.5*ds
+##   data1[i] <- 1 - num2[i]/num1[i]
+## }
 
-datafile1 <- sprintf("%s%02d%s", "prodiv", 10, ".dat")
-datafile2 <- sprintf("%s%02d%s", "prodiv", 11, ".dat")
-z1 <- matrix(scan(datafile1, n=NCollect2*1, quiet=TRUE),
-            NCollect2, 1, byrow=TRUE)
-z2 <- matrix(scan(datafile2, n=NCollect2*1, quiet=TRUE),
-            NCollect2, 1, byrow=TRUE)
-for (i in seq(M)){
-  num1[i] <- 0
-  num2[i] <- 0
-}
-for (j in seq(NCollect2)){
-    if ( z1[j,1] > 0 ) {
-      z1[j,1]  = min(us, z1[j,1])
-      z1[j,1] = max(ls, z1[j,1])         
-      Mindex <- floor( (z1[j,1]-ls)/ds ) + 1
-      num1[Mindex] = num1[Mindex] + 1
-      if (z2[j,1] > 0 ) {
-        num2[Mindex] = num2[Mindex] + 1
-      }
-    }
-  }
-for ( i in seq(M) ) {
-  data2[i] <- 1 - num2[i]/num1[i]
-}
+## datafile1 <- sprintf("%s%02d%s", "prodiv", 10, ".dat")
+## datafile2 <- sprintf("%s%02d%s", "prodiv", 11, ".dat")
+## z1 <- matrix(scan(datafile1, n=NCollect2*1, quiet=TRUE),
+##             NCollect2, 1, byrow=TRUE)
+## z2 <- matrix(scan(datafile2, n=NCollect2*1, quiet=TRUE),
+##             NCollect2, 1, byrow=TRUE)
+## for (i in seq(M)){
+##   num1[i] <- 0
+##   num2[i] <- 0
+## }
+## for (j in seq(NCollect2)){
+##     if ( z1[j,1] > 0 ) {
+##       z1[j,1]  = min(us, z1[j,1])
+##       z1[j,1] = max(ls, z1[j,1])         
+##       Mindex <- floor( (z1[j,1]-ls)/ds ) + 1
+##       num1[Mindex] = num1[Mindex] + 1
+##       if (z2[j,1] > 0 ) {
+##         num2[Mindex] = num2[Mindex] + 1
+##       }
+##     }
+##   }
+## for ( i in seq(M) ) {
+##   data2[i] <- 1 - num2[i]/num1[i]
+## }
 
-p1 <- xyplot(data1[1:(M-3)]~x[1:(M-3)],
-             xlab = list("s (cell size, fl)", cex=2),
-             ylab = list("Proportion of divisions", cex=2),
-             scales=list(cex=2),
-             type = 'b', lwd = 2, col="blue",
-             ylim = c(0, 1),
-             xlim = c(1400, 2600),
-             grid=TRUE,
-             cex = 1.5,
-             pch = 20,
-             panel = function(...){
-               panel.xyplot(...)
-               panel.lines(x[1:(M-3)], data2[1:(M-3)], 
-                         lwd=2, type='b', col='red', cex=1.5, pch=20)
-             }
-             )
-print(p1)
-dev.off()
+## p1 <- xyplot(data1[1:(M-3)]~x[1:(M-3)],
+##              xlab = list("s (cell size, fl)", cex=2),
+##              ylab = list("Proportion of divisions", cex=2),
+##              scales=list(cex=2),
+##              type = 'b', lwd = 2, col="blue",
+##              ylim = c(0, 1),
+##              xlim = c(1400, 2600),
+##              grid=TRUE,
+##              cex = 1.5,
+##              pch = 20,
+##              panel = function(...){
+##                panel.xyplot(...)
+##                panel.lines(x[1:(M-3)], data2[1:(M-3)], 
+##                          lwd=2, type='b', col='red', cex=1.5, pch=20)
+##              }
+##              )
+## print(p1)
+## dev.off()
 
 
 ## ## Trajecotries in the second paper

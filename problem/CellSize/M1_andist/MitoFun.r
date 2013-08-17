a00113b42d773e9385c6daffc091c16b1f3ed35c
hydrays <hydrays@gmail.com>
2013-08-17 13:07:47 +0000

    CellSize: added a new directory M1_andist.

---
+++ problem/CellSize/M1_andist/MitoFun.r
@@ -0,0 +1,66 @@
+#######################################
+## Error function for optimazation
+#######################################
+
+MitoFun <- function(mpv){
+
+  require(seewave)
+  if (length(mpv) != 2 ) {
+    cat('Number of parameters must equal to p\n')
+  }
+
+  ## Prepare control file
+  mp1 <- mpv[1]
+  mp2 <- mpv[2]
+
+  Content <- readLines('control.txt')
+  Content[24] <- paste('\tmp1 = ', mp1, ',')
+  Content[25] <- paste('\tmp2 = ', mp2)
+  writeLines(Content, 'control.txt')
+  
+  cat('Invoking Fortran program\n')
+  system('run > log')
+
+  parainfo <- read.csv("out/control.csv", strip.white=TRUE)
+  NPool <- parainfo$VALUE[parainfo$PARAMETER=='NPool']
+  
+  ## Asyn part
+  i <- 10
+  datafile <- sprintf("%s%05d%s", "out/m", i, ".dat")
+  z <- matrix(scan(datafile, n=NPool*4, quiet=TRUE),
+              NPool, 4, byrow=TRUE)
+  d1 <- density(z[,1])
+  SimResultA <- spline(d1, n=101, xmin=0, xmax=3000)
+  SimResultA$y <- pmax(0, SimResultA$y)
+  SimResultA$y <- SimResultA$y/(sum(SimResultA$y)*30)
+  ExpResultA <- read.csv('asyn_dist.csv')
+  ## L1 norm
+  ## ErrorA <- norm(as.matrix(ExpResultA$y-SimResultA$y))
+  ## KL divgence
+  tt1<-cbind(ExpResultA$x, ExpResultA$y)
+  tt2<-cbind(SimResultA$x, SimResultA$y)
+  ErrorA <- kl.dist(tt1,tt2)$D1
+  
+  ## Newborn part
+  datafile <- sprintf("%s%05d%s", "out/n", i, ".dat")
+  z <- matrix(scan(datafile, n=NPool*1, quiet=TRUE),
+              NPool, 1, byrow=TRUE)
+  d2 <- density(z[,1])
+  SimResultB <- spline(d2, n=101, xmin=0, xmax=3000)
+  SimResultB$y <- pmax(0, SimResultB$y)
+  SimResultB$y <- SimResultB$y/(sum(SimResultB$y)*30)
+  ExpResultB <- read.csv('newborn_dist.csv')
+  ## L1 norm
+  ## ErrorB <- norm(as.matrix(ExpResultB$y-SimResultB$y))
+  ## KL divgence
+  tt1<-cbind(ExpResultB$x, ExpResultB$y)
+  tt2<-cbind(SimResultB$x, SimResultB$y)
+  ErrorB <- kl.dist(tt1,tt2)$D1
+  
+  ## Total error
+  ErrorT <- ErrorA + ErrorB
+
+  ##cat('Para:', mp1, mp2, '-> Error:', ErrorT, '\n')
+  print(c(mp1, mp2, ErrorT), digits=16)
+  return(ErrorT)
+}
---
+++ problem/CellSize/M1_andist/out/mitosis_fit.r
@@ -0,0 +1,77 @@
+#######################################
+## Asynchronized cell size distribution
+#######################################
+
+require(seewave)
+ExpResultA <- read.csv('asyn_dist.csv')
+ExpResultB <- read.csv('newborn_dist.csv')
+
+parainfo <- read.csv("control.csv", strip.white=TRUE)
+NPool <- parainfo$VALUE[parainfo$PARAMETER=='NPool']
+NCollect <- parainfo$VALUE[parainfo$PARAMETER=='NCollect']
+
+## ## Use stable distribution as a reference
+## i <- 10
+## datafile <- sprintf("%s%05d%s", "m", i, ".dat")
+## z <- matrix(scan(datafile, n=NPool*4, quiet=TRUE),
+##             NPool, 4, byrow=TRUE)
+## d1 <- density(z[,1])
+## ExpResultA <- spline(d1, n=101, xmin=0, xmax=3000)
+## ExpResultA$y <- pmax(0, ExpResultA$y)
+## ExpResultA$y <- ExpResultA$y/(sum(ExpResultA$y)*30)
+## datafile <- sprintf("%s%05d%s", "n", i, ".dat")
+## z <- matrix(scan(datafile, n=NCollect*1, quiet=TRUE),
+##             NCollect, 1, byrow=TRUE)
+## d2 <- density(z[,1])
+## ExpResultB <- spline(d2, n=101, xmin=0, xmax=3000)
+## ExpResultB$y <- pmax(0, ExpResultB$y)
+## ExpResultB$y <- ExpResultB$y/(sum(ExpResultB$y)*30)
+
+#for (i in seq(10)) {
+
+i <- 10
+outfile <- sprintf("%s%05d%s", "asynew", i, ".png")
+png(outfile, width=600, height=800)
+#pdf(outfile, width=.pwidth, height=.pheight)
+#pdf("fasyn.pdf", width=7, height=5)
+
+par(mfrow=c(2,1))
+
+datafile <- sprintf("%s%05d%s", "m", i, ".dat")
+z <- matrix(scan(datafile, n=NPool*4, quiet=TRUE),
+            NPool, 4, byrow=TRUE)
+d1 <- density(z[,1])
+plot(d1, xlim=c(0,3000), ylim=c(0, 0.0015))
+
+SimResultA <- spline(d1, n=101, xmin=0, xmax=3000)
+SimResultA$y <- pmax(0, SimResultA$y)
+SimResultA$y <- SimResultA$y/(sum(SimResultA$y)*30)
+lines(SimResultA$x, SimResultA$y, type='l', col='red')
+
+lines(ExpResultA$x, ExpResultA$y, type='l', col='blue')
+## ErrorA[i] <- norm(as.matrix(ExpResultA$y-SimResultA$y), type='O')
+tt1<-cbind(ExpResultA$x, ExpResultA$y)
+tt2<-cbind(SimResultA$x, SimResultA$y)
+ErrorA <- kl.dist(tt1,tt2)$D1
+
+# Newborn part
+datafile <- sprintf("%s%05d%s", "n", i, ".dat")
+z <- matrix(scan(datafile, n=NCollect*1, quiet=TRUE),
+            NCollect, 1, byrow=TRUE)
+d2 <- density(z[,1])
+plot(d2, xlim=c(0,3000), ylim=c(0, 0.003))
+
+SimResultB <- spline(d2, n=101, xmin=0, xmax=3000)
+SimResultB$y <- pmax(0, SimResultB$y)
+SimResultB$y <- SimResultB$y/(sum(SimResultB$y)*30)
+lines(SimResultB$x, SimResultB$y, type='l', col='red')
+
+lines(ExpResultB$x, ExpResultB$y, type='l', col='blue')
+## ErrorB[i] <- norm(as.matrix(ExpResultB$y-SimResultB$y), type='O')
+tt1<-cbind(ExpResultB$x, ExpResultB$y)
+tt2<-cbind(SimResultB$x, SimResultB$y)
+ErrorB <- kl.dist(tt1,tt2)$D1
+
+ErrorT <- ErrorA + ErrorB
+dev.off()
+#}
